bb50cb0857a6485e769a63860eea61e3cddd3813
walkor <worker-man@qq.com>
2014-03-03 22:31:22 +0800

    首页排版

--- applications/Workerman.net/Views/workerman.tpl.php
+++ applications/Workerman.net/Views/workerman.tpl.php
@@ -3,25 +3,25 @@
 		<div class="col-md-12 column">
 			<nav class="navbar navbar-default" role="navigation">
 				<div class="navbar-header">
-					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only"><img src="http://static.yiiframework.com/css/img/logo.png" /></span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">Brand</a>
+					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only"></span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">Brand</a>
 				</div>
 				
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav">
 						<li class="active">
-							<a href="#">首页</a>
+							<a href="/">首页</a>
 						</li>
 						<li class="dropdown">
-							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">产品<strong class="caret"></strong></a>
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">相关应用<strong class="caret"></strong></a>
 							<ul class="dropdown-menu">
 								<li>
-									<a href="/workerman_thriftrpc">Thrift远程调用服务框架</a>
+									<a href="/workerman_thriftrpc">Thrift Rpc</a>
 								</li>
 								<li>
-									<a href="/workerman_jsonrpc">Json远程调用服务框架</a>
+									<a href="/workerman_jsonrpc">Json Rpc</a>
 								</li>
 								<li>
-									<a href="/workerman_statistics">分布式统计监控系统</a>
+									<a href="/workerman_statistics">分布式监控系统</a>
 								</li>
 								<li>
 									<a href="#">聊天室</a>
@@ -57,10 +57,11 @@
 								<img alt="" src="http://lorempixel.com/1600/500/sports/1" />
 								<div class="carousel-caption">
 									<h4>
-										First Thumbnail label
+										Workerman-ThriftRpc
 									</h4>
 									<p>
-										Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+										<strong>Workerman作为进程管理器，并结合Thrift的一款Rpc远程调用框架。</strong>
+										内置统计监控模块，支持平滑重启，支持故障ip自动踢出及恢复等诸多特性。
 									</p>
 								</div>
 							</div>
@@ -68,10 +69,11 @@
 								<img alt="" src="http://lorempixel.com/1600/500/sports/2" />
 								<div class="carousel-caption">
 									<h4>
-										Second Thumbnail label
+										Workerman-Statistics
 									</h4>
 									<p>
-										Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+										<strong>一款分布式统计监控系统</strong>
+										以曲线图和表格的方式展示统计数据如调用量、延迟、成功率、及错误日志等。
 									</p>
 								</div>
 							</div>
@@ -79,10 +81,11 @@
 								<img alt="" src="http://lorempixel.com/1600/500/sports/3" />
 								<div class="carousel-caption">
 									<h4>
-										Third Thumbnail label
+									Workerman-JsonRpc
 									</h4>
 									<p>
-										Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+									<strong>以Json作为协议的一款高性能Rpc远程调用框架</strong>
+										开发快、性能高、内置统计模块。
 									</p>
 								</div>
 							</div>
@@ -91,13 +94,14 @@
 				</div>
 				<div class="col-md-6 column">
 					<h2>
-						Heading
+						什么是Workerman
 					</h2>
 					<p>
-						Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+						Workerman是一个高性能的PHP socket 服务器框架。已经被多家公司用于Rpc服务器、聊天室服务器、网络游戏服务器、手机游戏等后台开发。
+						只要你会PHP，你就可以基于Workerman轻而易举的开发出你想要的网络应用，不必再为底层Socket开发而烦恼。
 					</p>
 					<p>
-						<a class="btn" href="#">View details »</a>
+						<a class="btn" href="#">查看更多 »</a>
 					</p>
 				</div>
 			</div>
@@ -107,10 +111,10 @@
 						<img alt="300x200" src="http://lorempixel.com/600/200/people" />
 						<div class="caption">
 							<h3>
-								Thumbnail label
+								高性能
 							</h3>
 							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+								基于libevent事件轮询库，支持每秒4W的吞吐量，支持几万的并发连接。
 							</p>
 							<p>
 								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
@@ -123,10 +127,10 @@
 						<img alt="300x200" src="http://lorempixel.com/600/200/city" />
 						<div class="caption">
 							<h3>
-								Thumbnail label
+								高可用
 							</h3>
 							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+								Wokerman已经被很多公司证实其高可用性，这些公司不乏日营业额过亿的电子商务公司用于订单系统的开发，以及知名网游公司用于游戏后台的开发。
 							</p>
 							<p>
 								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
@@ -139,10 +143,11 @@
 						<img alt="300x200" src="http://lorempixel.com/600/200/sports" />
 						<div class="caption">
 							<h3>
-								Thumbnail label
+								开发简单
 							</h3>
 							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+								Workerman已经有很多成熟的网络应用，只要你会PHP，你就可以在这些应用框架的基础上，快速的开发出自己的Socket应用服务程序。
+								使用Workerman，你无需安装PHP-FPM、Apache或者Nginx，只要你安装了PHP就可以直接运行。
 							</p>
 							<p>
 								<a class="btn btn-primary" href="#">Action</a> <a class="btn" href="#">Action</a>
@@ -154,53 +159,53 @@
 			<div class="row clearfix">
 				<div class="col-md-6 column">
 					<h3>
-						h3. Lorem ipsum dolor sit amet.
+						基于Workerman开发的应用程序
 					</h3>
 					<div class="media">
 						 <a href="#" class="pull-left"><img src="http://lorempixel.com/64/64/" class="media-object" alt='' /></a>
 						<div class="media-body">
 							<h4 class="media-heading">
-								Nested media heading
-							</h4> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-							<div class="media">
-								 <a href="#" class="pull-left"><img src="http://lorempixel.com/64/64/" class="media-object" alt='' /></a>
-								<div class="media-body">
-									<h4 class="media-heading">
-										Nested media heading
-									</h4> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
-								</div>
-							</div>
+								Workerman-ThriftRpc
+							</h4>
+							 Workerman和Thrift框架相结合开发的一款Rpc框架，目的是解决异构系统与PHP通信的问题。使用Thrift你可以方便的用各种语言调用PHP提供的服务了。
+						</div>
+					</div>
+					<div class="media">
+						 <a href="#" class="pull-left"><img src="http://lorempixel.com/64/64/" class="media-object" alt='' /></a>
+						<div class="media-body">
+							<h4 class="media-heading">
+								Workerman-JsonRpc
+							</h4>
+							 使用Workerman以Json作为协议的一个轻量级高性能的Rpc框架。JsonRpc的目标是简单、快速。开发者只要写好需要的类和方法，使用提供的客户端便可以方便的远程调用。
+						</div>
+					</div>
+					<div class="media">
+						 <a href="#" class="pull-left"><img src="http://lorempixel.com/64/64/" class="media-object" alt='' /></a>
+						<div class="media-body">
+							<h4 class="media-heading">
+								Workerman-Statistic
+							</h4>
+							Workerman-Statistic是一款分布式统计监控系统，纯PHP开发、无需安装Mysql、Nginx、Apache等软件，只要你安装了PHP就能直接运行。Workerman-Statistic目的是用来监控系统关键节点运行情况，包括调用量、耗时、成功率、错误日志。
+							最后以曲线图和表格的形式将这些数据展示出来。
 						</div>
 					</div>
 				</div>
 				<div class="col-md-6 column">
 					<h3>
-						h3. Lorem ipsum dolor sit amet.
+						最近更新
 					</h3>
 					<ul>
 						<li>
-							Lorem ipsum dolor sit amet
+							2014-02-25 开始开发基于Websocket的聊天室
 						</li>
 						<li>
-							Consectetur adipiscing elit
+							2014-01-03 分布式监控系统发布
 						</li>
 						<li>
-							Integer molestie lorem at massa
+							2013-12-05 Thrif Rpc框架发布
 						</li>
 						<li>
-							Facilisis in pretium nisl aliquet
-						</li>
-						<li>
-							Nulla volutpat aliquam velit
-						</li>
-						<li>
-							Faucibus porta lacus fringilla vel
-						</li>
-						<li>
-							Aenean sit amet erat nunc
-						</li>
-						<li>
-							Eget porttitor lorem
+							2013-11-11 Json Rpc 框架发布
 						</li>
 					</ul>
 				</div>
