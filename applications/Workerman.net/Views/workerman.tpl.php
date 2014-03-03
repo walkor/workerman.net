233b90c1c403ec5654ed1d59e4b43444abf04595
walkor <worker-man@qq.com>
2014-03-03 23:36:05 +0800

    页面排版 提交

--- applications/Workerman.net/Views/workerman.tpl.php
+++ applications/Workerman.net/Views/workerman.tpl.php
@@ -43,88 +43,50 @@
 			</nav>
 			<div class="row clearfix">
 				<div class="col-md-6 column">
-					<div class="carousel slide" id="carousel-437921">
-						<ol class="carousel-indicators">
-							<li class="active" data-slide-to="0" data-target="#carousel-437921">
-							</li>
-							<li data-slide-to="1" data-target="#carousel-437921">
-							</li>
-							<li data-slide-to="2" data-target="#carousel-437921">
-							</li>
-						</ol>
-						<div class="carousel-inner">
-							<div class="item active">
-								<img alt="" src="http://lorempixel.com/1600/500/sports/1" />
-								<div class="carousel-caption">
-									<h4>
-										Workerman-ThriftRpc
-									</h4>
-									<p>
-										<strong>Workerman结合Thrift</strong>
-										内置统计监控模块，支持平滑重启，支持故障ip自动踢出。
-									</p>
-								</div>
-							</div>
-							<div class="item">
-								<img alt="" src="http://lorempixel.com/1600/500/sports/2" />
-								<div class="carousel-caption">
-									<h4>
-										Workerman-Statistics
-									</h4>
-									<p>
-										<strong>一款分布式统计监控系统</strong>
-										以曲线图和表格的方式展示统计数据如调用量、延迟、成功率、及错误日志等。
-									</p>
-								</div>
-							</div>
-							<div class="item">
-								<img alt="" src="http://lorempixel.com/1600/500/sports/3" />
-								<div class="carousel-caption">
-									<h4>
-									Workerman-JsonRpc
-									</h4>
-									<p>
-									<strong>以Json作为协议的一款高性能Rpc远程调用框架</strong>
-										开发快、性能高、内置统计模块。
-									</p>
-								</div>
-							</div>
-						</div> <a class="left carousel-control" href="#carousel-437921" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-437921" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-					</div>
-				</div>
-				<div class="col-md-6 column">
 					<h2>
 						什么是Workerman
 					</h2>
 					<p>
 						Workerman是一个高性能的PHP socket 服务器框架。已经被多家公司用于Rpc服务器、聊天室服务器、网络游戏服务器、手机游戏等后台开发。
-						只要你会PHP，你就可以基于Workerman轻而易举的开发出你想要的网络应用，不必再为底层Socket开发而烦恼。
+						只要会PHP，你就可以基于Workerman轻而易举的开发出你想要的网络应用，不必再为底层Socket开发而烦恼。
 					</p>
 					<p>
 						<a class="btn" href="#">查看更多 »</a>
 					</p>
 				</div>
+				<div class="col-md-6 column">
+					<ol>
+						<li>使用PHP开发，多进程支持</li>
+						<li>支持TCP/UDP，支持TCP长连接</li>
+						<li>使用libevent事件轮询库，支持高并发</li>
+						<li>支持服务器平滑重启</li>
+						<li>支持文件更新检测</li>
+				</ol>
+				</div>
+			</div>
+			<div class="row clearfix">
+				<div class="col-md-12 column">
+					<img src="http://static.zend.com/topics/homepage-Zend-Empowers-the-Agile-Enterprise-banner.png" />
+				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-4">
 					<div class="thumbnail">
-						<img alt="300x200" src="http://lorempixel.com/600/200/people" />
 						<div class="caption">
 							<h3>
-								高性能
+								<img src="http://static.zend.com/topics//hp-icon-agile-design.png" />高性能
 							</h3>
 							<p>
-								支持多进程，支持TCP/UDP，基于高性能的libevent事件轮询库，支持每秒4W的吞吐量，支持几万的并发连接。
+								基于高性能的libevent事件轮询库，支持每秒4W的吞吐量，支持几万的并发连接。数据库等对象可以常驻内存，减少解析编译开销。
 							</p>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="thumbnail">
-						<img alt="300x200" src="http://lorempixel.com/600/200/city" />
 						<div class="caption">
 							<h3>
-								高可用
+								<img src="http://static.zend.com/topics//hp-icon-continuous-delivery.png" />高可用
 							</h3>
 							<p>
 								已被多家公司证实其高可用性，这些公司不乏日营业额过亿的电子商务公司用于订单系统的开发，以及知名网游公司用于游戏后台的开发。
@@ -134,13 +96,12 @@
 				</div>
 				<div class="col-md-4">
 					<div class="thumbnail">
-						<img alt="300x200" src="http://lorempixel.com/600/200/sports" />
 						<div class="caption">
 							<h3>
-								开发简单
+								<img src="http://static.zend.com/topics//hp-icon-cloud-scale.png" />开发简单
 							</h3>
 							<p>
-								Workerman已经有很多成熟的网络应用，只要你会PHP，你就可以在这些应用框架的基础上，快速的开发出自己的Socket应用服务程序。
+								Workerman已经有很多成熟的网络应用，只要你会PHP，你就可以在这些应用的基础上快速的开发出自己的Socket应用服务。
 							</p>
 						</div>
 					</div>
