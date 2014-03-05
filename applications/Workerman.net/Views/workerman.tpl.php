ecead3c7b5df3d7b443d356777dabb6d443473ff
walkor <worker-man@qq.com>
2014-03-05 22:25:09 +0800

    样式

--- applications/Workerman.net/Views/workerman.tpl.php
+++ applications/Workerman.net/Views/workerman.tpl.php
@@ -46,7 +46,7 @@
 					<h2>
 						什么是Workerman
 					</h2>
-					<p>
+					<p class="f14">
 						Workerman是一个<strong>高性能</strong>的<strong>PHP socket 服务器框架</strong>。已经被多家公司用于Rpc服务器、聊天室服务器、网络游戏服务器、手机游戏等后台开发。
 						只要会PHP，你就可以基于Workerman轻而易举的开发出你想要的网络应用，不必再为底层Socket开发而烦恼。
 					</p>
