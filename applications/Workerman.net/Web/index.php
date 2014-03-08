2ec6bd6c7e7cb555b0b35ad2349cc6114555c7e4
walkor <worker-man@qq.com>
2014-03-08 22:17:50 +0800

    index.php 判断url path

--- applications/Workerman.net/Web/index.php
+++ applications/Workerman.net/Web/index.php
@@ -3,10 +3,22 @@ require_once WORKERMAN_ROOT_DIR .'applications/Workerman.net/Web/_init.php';
 
 $url_info = parse_url($_SERVER['REQUEST_URI']);
 $path = isset($url_info['path']) ? $url_info['path'] : 'workerman';
-$func = str_replace('/', '', $path);
-if(empty($func))
+$tmp_arr = explode('/', $path);
+foreach($tmp_arr as $key=>$value)
 {
-    $func = 'workerman';
+    if(trim($value) == '')
+    {
+        unset($tmp_arr[$key]);
+    }
+}
+$tmp_arr = array_values($tmp_arr);
+if(isset($tmp_arr[0]))
+{
+    $func = "\\Workerman\\Modules\\{$tmp_arr[0]}";
+    unset($tmp_arr[0]);
+}else 
+{
+    $func = "\\Workerman\\Modules\\workerman";
 }
 if(!function_exists($func))
 {
@@ -21,4 +33,4 @@ if(!function_exists($func))
     $func = "\\Workerman\\Modules\\workerman";
 }
 
-call_user_func_array($func, array());
+call_user_func_array($func, $tmp_arr);
