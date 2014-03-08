32ad3abb9c254f48f567631d414cf6c4db27ab20
walkor <worker-man@qq.com>
2014-03-08 20:51:12 +0800

    favicon

---
+++ applications/Workerman.net/Modules/applications.php
@@ -0,0 +1,10 @@
+<?php
+namespace WorkerMan\Modules;
+
+function applications()
+{
+    
+    include NET_ROOT . '/Views/header.tpl.php';
+    include NET_ROOT . '/Views/workerman.tpl.php';
+    include NET_ROOT . '/Views/footer.tpl.php';
+}
