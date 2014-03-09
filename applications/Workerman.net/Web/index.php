3d8d06a9019f422ed1f66b8689cbcf908c8cabeb
walkor <worker-man@qq.com>
2014-03-09 17:21:20 +0800

    index.php 永久重定向 /worlerman 到首页

--- applications/Workerman.net/Web/index.php
+++ applications/Workerman.net/Web/index.php
@@ -2,7 +2,20 @@
 require_once WORKERMAN_ROOT_DIR .'applications/Workerman.net/Web/_init.php';
 
 $url_info = parse_url($_SERVER['REQUEST_URI']);
-$path = isset($url_info['path']) ? $url_info['path'] : 'workerman';
+if(isset($url_info['path']))
+{
+    if($url_info['path'] == 'workerman')
+    {
+        \App\Common\Protocols\Http\header('Location: /'.(!empty($_GET) ? '?'.http_build_query($_GET) : ''));
+        return \App\Common\Protocols\Http\header('HTTP/1.1 301 Moved Permanently');
+    }
+    $path = $url_info['path'];
+}
+else
+{
+    $path = 'workerman';
+}
+    
 $tmp_arr = explode('/', $path);
 foreach($tmp_arr as $key=>$value)
 {
@@ -15,6 +28,11 @@ $tmp_arr = array_values($tmp_arr);
 if(isset($tmp_arr[0]))
 {
     $func = "\\Workerman\\Modules\\{$tmp_arr[0]}";
+    if(isset($tmp_arr[1]))
+    {
+        $func .= '_'.$tmp_arr[1];
+        unset($tmp_arr[1]);
+    }
     unset($tmp_arr[0]);
 }else 
 {
@@ -30,6 +48,7 @@ if(!function_exists($func))
 
 if(!function_exists($func))
 {
+    return \App\Common\Protocols\Http\header('Location :/');
     $func = "\\Workerman\\Modules\\workerman";
 }
 
