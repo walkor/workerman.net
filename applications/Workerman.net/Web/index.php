d1ff560b15565812cf9faa2c36ad8d1c233d20a6
walkor <worker-man@qq.com>
2014-03-09 17:28:52 +0800

    /aaa/bbb/ccc/ddd => function aaa_bbb(ccc, ddd, ..)

--- applications/Workerman.net/Web/index.php
+++ applications/Workerman.net/Web/index.php
@@ -4,18 +4,13 @@ require_once WORKERMAN_ROOT_DIR .'applications/Workerman.net/Web/_init.php';
 $url_info = parse_url($_SERVER['REQUEST_URI']);
 if(isset($url_info['path']))
 {
-    if($url_info['path'] == 'workerman')
-    {
-        \App\Common\Protocols\Http\header('Location: /'.(!empty($_GET) ? '?'.http_build_query($_GET) : ''));
-        return \App\Common\Protocols\Http\header('HTTP/1.1 301 Moved Permanently');
-    }
     $path = $url_info['path'];
 }
 else
 {
-    $path = 'workerman';
+    $path = '';
 }
-    
+
 $tmp_arr = explode('/', $path);
 foreach($tmp_arr as $key=>$value)
 {
@@ -27,6 +22,11 @@ foreach($tmp_arr as $key=>$value)
 $tmp_arr = array_values($tmp_arr);
 if(isset($tmp_arr[0]))
 {
+    if($tmp_arr[0] == 'workerman')
+    {
+        \App\Common\Protocols\Http\header('Location: /'.(!empty($_GET) ? '?'.http_build_query($_GET) : ''));
+        return \App\Common\Protocols\Http\header('HTTP/1.1 301 Moved Permanently');
+    }
     $func = "\\Workerman\\Modules\\{$tmp_arr[0]}";
     if(isset($tmp_arr[1]))
     {
