5cee56588a7d9c714f6f1c8d59cc5f07f4591610
walkor <worker-man@qq.com>
2014-03-09 20:45:28 +0800

    下载次数

--- applications/Workerman.net/Modules/download.php
+++ applications/Workerman.net/Modules/download.php
@@ -17,6 +17,6 @@ function download_workerman($file_type = '')
     $download_workerman_count_key = 'download_workerman_count';
     $count = \WorkerMan\Lib\Store::get($download_workerman_count_key);
     $count = $count >= 0 ? intval($count) : 0;
-    \WorkerMan\Lib\Store::set($download_workerman_count_key, $count++);
+    \WorkerMan\Lib\Store::set($download_workerman_count_key, ++$count);
     return \App\Common\Protocols\Http\header('Location: https://github.com/walkor/workerman/archive/master.zip');
 }
