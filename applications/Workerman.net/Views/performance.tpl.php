654c60030f8b5c9375dadf84c6c26ba87f198431
walkor <worker-man@qq.com>
2014-03-19 22:18:07 +0800

    performance 样式

--- applications/Workerman.net/Views/performance.tpl.php
+++ applications/Workerman.net/Views/performance.tpl.php
@@ -8,25 +8,22 @@
 </ul>
 <div class="row clearfix">
 	<div class="col-md-12 column">
-		<h2>
-			高性能的workerman
-		</h2>
-		<p>
-			workerman是一个高性能的php socket 服务器框架。下图说明了workerman的运行机制。
-		</p>
-	</div>
+	<h3>
+		为什么workerman拥有卓越的性能
+	</h3>
 	<div class="text-center"><img src="/img/workerman-request-map.png" alt="workerman 运行机制" /></div>
 	<div class="thumbnail">
 		<div class="caption">
-			<h3>
-				为什么workerman拥有卓越的性能
-			</h3>
 			<h4>避免读取磁盘和反复编译</h4>
 			<p>
     			workerman运行过程中，单个进程生命周期内只要PHP文件被载入编译过一次，便会常驻内存，不会再去从磁盘读取或者再去编译。
     			workerman省去了每次请求中的请求初始化、创建执行环境、词法解析、语法解析、编译生成opcode以及请求关闭等诸多耗时的步骤。
     			实际上workerman运行起来后便几乎没有磁盘IO及PHP文件编译开销，剩下的只是简单的计算过程，这使得workerman运行飞快。
 			</p>
+			</div>
+	</div>
+	<div class="thumbnail">
+		<div class="caption">
 			<h4>数据或者资源可以全局共享</h4>
 			<p>
     			workerman中多个请求是可以共享数据或者资源的，当前请求产生的全局变量或者类的静态成员在下一次请求中仍然有效。
@@ -34,6 +31,10 @@
     			从而不必每次用户请求都去创建新的连接，避免了连接时三次握手、连接后权限验证以及断开连接时四次握手等耗时的交互过程。不仅数据库，像redis、
     			memcache等同样有效。少了这些开销和网络交互，使得workerman运行更快。
 			</p>
+			</div>
+	</div>
+	<div class="thumbnail">
+		<div class="caption">
 			<h4>没有多余的网络开销</h4>
 			<p>
 			    传统PHP应用程序需要借助apache、nginx等容器才能对外提供网络服务，这就导致多了一层apache、nginx等容器到PHP进程的网络传输开销。
@@ -62,5 +63,5 @@ cpu：Intel® Core™ i3-3220 CPU @ 3.30GHz × 4
 结果： 吞吐量：<b>9.7W/S</b> ， cpu：68% ， 内存占用：4*8M = 32M
 </code></pre>
 <pre><code>无流量抖动，无内存泄漏，性能非常强悍</code></pre>
-	
+	</div>
 </div>
