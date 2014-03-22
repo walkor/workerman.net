4eab7f7cd648b67d3c83c81bbfdc1ffe6fcfef79
walkor <worker-man@qq.com>
2014-03-22 11:45:03 +0800

    百度统计、

--- applications/Workerman.net/Views/footer.tpl.php
+++ applications/Workerman.net/Views/footer.tpl.php
@@ -35,5 +35,9 @@
 		</div>
 	</div>
 </div>
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F609dc2f866ce0133cdf405ea1a45f492' type='text/javascript'%3E%3C/script%3E"));
+</script>
 </body>
 </html>
