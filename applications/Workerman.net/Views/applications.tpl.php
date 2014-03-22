e6ea0049bcd686a773fffb0604e50370d682f44a
walkor <worker-man@qq.com>
2014-03-22 10:43:08 +0800

    应用

--- applications/Workerman.net/Views/applications.tpl.php
+++ applications/Workerman.net/Views/applications.tpl.php
@@ -7,26 +7,26 @@
 	 <a href="/workerman-thrift" class="pull-left"><img src="/img/workerman-thrift.png" class="media-object" alt='' /></a>
 	<div class="media-body">
 		<h4 class="media-heading">
-			Thrift RPC远程调用框架
+			<a target="_blank" href="/workerman-thrift">Thrift RPC远程调用框架</a>
 		</h4>
-		 Workerman和Thrift框架相结合，目的是解决异构系统与PHP通信的问题，使你的服务更方便的被其它语言调用。
+		 <p>Workerman和Thrift框架相结合，目的是解决异构系统与PHP通信的问题，使你的服务更方便的被其它语言调用。<a target="_blank" class="btn" href="/workerman-thrift">详情 »</a></p>
 	</div>
 </div>
 <div class="media">
 	 <a href="/workerman-chat" class="pull-left"><img src="/img/workerman-chat.png" class="media-object" alt='' /></a>
 	<div class="media-body">
 		<h4 class="media-heading">
-			聊天室
+			<a target="_blank" href="/workerman-chat">聊天室</a>
 		</h4>
-		一个可扩展的基于Websocket协议的聊天室，服务端采用workerman作为Socket服务器框架，客户端支持多浏览器统一接口，支持浏览器原生Websocket同时支持Flash实现的Websocket。
+		<p>一个可扩展的基于Websocket协议的聊天室，服务端采用workerman作为Socket服务器框架，客户端支持多浏览器统一接口，支持浏览器原生Websocket同时支持Flash实现的Websocket。<a target="_blank" class="btn" href="/workerman-chat">详情 »</a></p>
 	</div>
 </div>
 <div class="media">
 	 <a href="/workerman-statistics" class="pull-left"><img src="/img/workerman-statistics.png" class="media-object" alt='' /></a>
 	<div class="media-body">
 		<h4 class="media-heading">
-			分布式统计系统
+			<a target="_blank" href="/workerman-statistics">分布式统计系统</a>
 		</h4>
-		基于workerman socket框架开发的一款分布式统计监控系统，纯PHP开发、无需安装Mysql、Nginx、Apache等软件，只要你安装了PHP就行，需要Linux系统哦。
+		<p>基于workerman socket框架开发的一款分布式统计监控系统，纯PHP开发、无需安装Mysql、Nginx、Apache等软件，只要你安装了PHP就行，需要Linux系统哦。<a target="_blank" class="btn" href="/workerman-statistics">详情 »</a></p>
 	</div>
 </div>
