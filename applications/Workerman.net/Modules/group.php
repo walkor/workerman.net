d5a7cf99a02e7a9e52bd8d609eb0fb3556ce0903
walkor <worker-man@qq.com>
2014-03-22 22:19:53 +0800

    documentation

--- applications/Workerman.net/Modules/group.php
+++ applications/Workerman.net/Modules/group.php
@@ -1,10 +1,10 @@
 <?php
 namespace WorkerMan\Modules;
 
-function documentation()
+function group()
 {
     $html_title = '社区 | workerman PHP Socket 服务器框架';
-    $html_nav = 'documentation';
+    $html_nav = 'group';
     include NET_ROOT . '/Views/header.tpl.php';
     include NET_ROOT . '/Views/building.tpl.php';
     include NET_ROOT . '/Views/footer.tpl.php';
