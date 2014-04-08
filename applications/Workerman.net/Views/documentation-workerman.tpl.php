351d0ef52ae044f2f998e8af84519818918f52a3
walkor <worker-man@qq.com>
2014-04-09 00:25:48 +0800

    ci

--- applications/Workerman.net/Views/documentation-workerman.tpl.php
+++ applications/Workerman.net/Views/documentation-workerman.tpl.php
@@ -54,7 +54,7 @@
 		安装libevent扩展将使得php可以使用Epoll等高级的事件管理机制，大大的提高程序性能。注意：如果未安装libevent扩展，workerman则使用PHP自带的Select事件管理机制。</p>
 		<h4>4、[可选安装]安装proctitle扩展</h4>
 		<p>PHP>=5.5原生集成可跳过此步。为了方便查看进程信息，建议安装proctitle扩展。安装proctitile扩展命令:<code>pecl install proctitle</code>。
-		使用protitl扩展可以用ps命令看到workerman详细的进程的详细信息包括服务名、ip、端口、协议等如下图:</p>
+		使用proctitle扩展可以用ps命令看到workerman详细的进程的详细信息包括服务名、ip、端口、协议等如下图:</p>
 		<img src="/img/ps-workerman-game.png" />
 		<h3><a name="install">下载安装</a></h3>
 		<p>下载后解压即可 <a href="/download/workermanzip" target="_blank">workerman.2.0.1.zip</a></p>
@@ -95,5 +95,23 @@ Escape character is '^]'.
 hello world
 hello world
 Connection closed by foreign host.</code></pre>
+    <h3><a name="interface">接口说明</a></h3>
+    <p>类<code>Man\Core\SocketWorker</code>预留了4个钩子函数，使得开发者可以在进程启动阶段、有数据到来时、数据接收完毕时、进程退出时加入自己的业务逻辑。</p>
+    <pre><code>bool public onStart();</code></pre>
+    <p><b>[可选实现]</b> 当业务进程启动的时候出发一次，这里可以做一些全局的初始化的工作，例如初始化配置、数据库连接等。</p>
+    <pre><code>bool public onStop();</code></pre>
+    <p><b>[可选实现]</b> 当业务进程停止服务准备退出时触发，这里可以做一些全局的收尾工作。引起进程停止服务准备退出的情况或者原因如下：
+    <br>1、运行停止命令 ./bin/workermand stop 
+    <br>2、运行重启命令 ./bin/workermand restart
+    <br>3、平滑重启服务 ./bin/workermand reload
+    <br>4、workerman检测到业务进程内存泄露达到上限值 conf/conf.d/Monitor.conf 中的max_mem_limit重启对应进程</p>
+    <pre><code>int public dealInput(string $bin);</code></pre>
+    <p><b>[必须实现]</b>当有数据到达时触发，参数$bin是接受到的全部数据（包括之前接收的未处理的数据）。由于网络延时、本地socket缓冲区大小限制、TCP分片等原因，
+    数据包可能不会一次全部到达，这时需要根据选定的协议和已经接收到的数据判断还有多少字节数据没有收到。返回0代表数据全部接收完毕（进入业务处理阶段），
+    返回x>0代表还有x字节没有收到（继续等待x字节的数据），返回false表示数据包有错（终止当前请求，等待其它请求），可以通过./bin/workermand status查看每个业务进程的请求量、包错误量等信息。
+    </p>
+    <pre><code>int public dealProcess(string $bin);</code></pre>
+    <p><b>[必须实现]</b>当数据全部接收完毕时（既dealInput()返回0时）触发。参数$bin是接收到的全部数据，根据选定协议解析出请求内容做相应的业务处理</p>
+    
 	</div>
 </div>
