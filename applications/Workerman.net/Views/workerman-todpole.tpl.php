3b17c8ff2ff3ceb2aebdad9696370e580c84cc05
walkor <worker-man@qq.com>
2014-05-28 23:19:07 +0800

    小蝌蚪详细安装教程

--- applications/Workerman.net/Views/workerman-todpole.tpl.php
+++ applications/Workerman.net/Views/workerman-todpole.tpl.php
@@ -62,19 +62,18 @@
 			<img src="/img/apt-install-php5-cli.png" alt="apt-get 安装 php5-cli"/>
 			<h4>3、终端运行<code>apt-get install git</code>如图：</h4>
 			<img src="/img/apt-install-git.png" alt="apt-get 安装 git"/>
-			<h4>4、终端运行<code>cd /home/</code></h4>
-			<h4>5、终端运行<code>git clone https://github.com/walkor/workerman-todpole</code>如图：</h4>
+			<h4>4、终端运行<code> cd /home/ && git clone https://github.com/walkor/workerman-todpole</code>如图：</h4>
 			<img src="/img/git-clone-workerman-todpole.png" alt="git 下载小蝌蚪代码"/>
-			<h4>6、终端运行<code>/home/workerman-todpole/workerman/bin/workermand start</code>如图</h4>
+			<h4>5、终端运行<code>/home/workerman-todpole/workerman/bin/workermand start</code>如图</h4>
 			<img src="/img/workerman-todpole-start-without-libevent.png" alt="php蝌蚪小游戏启动界面" />
-			<h4>7、大功告成！浏览器访问 http://ip:8383 如图:（如果不知道自己服务器ip，则运行 <code>ip addr | grep eth1 | grep inet</code>查看）</h4>
+			<h4>6、大功告成！浏览器访问 http://ip:8383 如图:（如果不知道自己服务器ip，则运行 <code>ip addr | grep eth1 | grep inet</code>查看）</h4>
 			<a href="http://workerman.net:8383" target="_blank"><img src="/img/workerman-todpole-browser.png" alt="php蝌蚪小游戏浏览器界面" /></a>
 			<br><br>
 			<h3>阿里云详细安装教程之centos系统</h3>
 			<h4>1、<code>yum install php-cli.x86_64 php-process.x86_64 git.x86_64</code></h4>
 			<h4>2、<code>cd /home/ && git clone https://github.com/walkor/workerman-todpole</code></h4>
 			<h4>3、<code>/home/workerman-todpole/workerman/bin/workermand start</code></h4>
-			<h4>7、大功告成！浏览器访问 http://ip:8383 （如果不知道自己服务器ip，则运行 <code>ip addr | grep eth1 | grep inet</code>查看）</h4>
+			<h4>4、大功告成！浏览器访问 http://ip:8383 （如果不知道自己服务器ip，则运行 <code>ip addr | grep eth1 | grep inet</code>查看）</h4>
 			<br>
 			<a href="http://s.click.taobao.com/t?e=m%3D2%26s%3DGKeTyTP6Ah8cQipKwQzePCperVdZeJviEViQ0P1Vf2kguMN8XjClAhbL5GVocYPX6vFecR8G%2FoXXm6P3uOIyfg86qmwSDD1wfViFRpBEm%2BxEiFpG%2BYiVROdn1BbglxZYxUhy8exlzcq9AmARIwX9K2Zg%2BdzdQFOwfMRvoxSVDSdLyrb2g0H2G5JcxXijM%2BwneEHpPTskRHnPKdU%2FdTrgjbw4MC6y5nKlXF%2B87KN7TKeiZ%2BQMlGz6FQ%3D%3D" target="_blank"><img src="http://gtms01.alicdn.com/tps/i1/T1jhmAFQhbXXb34_n6-468-60.jpg" /></a>
 			<br>
