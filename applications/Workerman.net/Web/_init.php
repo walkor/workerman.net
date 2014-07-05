473868d251416259b730fd619a99f7d2eed394dc
walkor <worker-man@qq.com>
2014-07-05 23:19:38 +0800

    ad

--- applications/Workerman.net/Web/_init.php
+++ applications/Workerman.net/Web/_init.php
@@ -1,3 +1,7 @@
 <?php
 define('NET_ROOT', realpath(dirname(__FILE__) . '/..'));
 require_once NET_ROOT . '/Lib/Store.php';
+
+global $bd_ad_250_250, $bd_ad_468_60;
+$bd_ad_250_250 = '<script type="text/javascript">/*250*250，创建于2014-7-5*/var cpro_id = "u1611641";</script><script src="http://cpro.baidustatic.com/cpro/ui/c.js" type="text/javascript"></script>';
+$bd_ad_468_60 = '<script type="text/javascript">/*468*60，创建于2014-7-5*/var cpro_id = "u1611676";</script><script src="http://cpro.baidustatic.com/cpro/ui/c.js" type="text/javascript"></script>';
