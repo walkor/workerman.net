d1097a7ee7b9ccbcbaaca28f6cf0e9b80bd64638
liangl <liangl@liangl.(none)>
2014-07-08 11:35:52 +0800

    install 安装

--- applications/Workerman.net/Views/install.tpl.php
+++ applications/Workerman.net/Views/install.tpl.php
@@ -9,7 +9,7 @@
 	<h5>5、命令行运行<code>/home/workerman-todpole/workerman/bin/workermand start</code></h5>
 	<br>
 	<h3>debian/ubuntu系统安装教程</h3>
-	<h5>1、命令行运行<code>apt-get update && apt-get install php5-cli git php-pear php5-dev libevent-dev</code></h5>
+	<h5>1、命令行运行<code>apt-get install php5-cli git php-pear php5-dev libevent-dev</code></h5>
 	<h5>2、命令行运行<code>pecl install channel://pecl.php.net/libevent-0.1.0</code></h5>
 	<h5>3、命令行运行<code>echo extension=libevent.so > /etc/php5/cli/conf.d/libevent.ini</code></h5>
 	<h5>4、命令行运行<code>cd /home/ && git clone https://github.com/walkor/workerman-todpole</code></h5>
