fcd47bdb21897d70c9434826878490add76b8e8b
walkor <worker-man@qq.com>
2014-07-18 23:23:23 +0800

    Webserver Chdir

--- workerman/Common/WebServer.php
+++ workerman/Common/WebServer.php
@@ -203,6 +203,8 @@ class WebServer extends Man\Core\SocketWorker
             // 如果请求的是php文件
             if($extension == 'php')
             {
+                $cwd = getcwd();
+                chdir($root_dir);
                 ini_set('display_errors', 'off');
                 // 缓冲输出
                 ob_start();
@@ -232,6 +234,7 @@ class WebServer extends Man\Core\SocketWorker
                 $this->sendToClient($buffer);
                 // 执行php每执行一次就退出(原因是有的业务使用了require_once类似的语句，不能重复加载业务逻辑)
                 //return $this->stop();
+                chdir($cwd);
                 return ;
             }
             
