8484e446bc00e4fe0917aa14366c013a38ff5ff5
walkor <worker-man@qq.com>
2014-07-18 22:46:50 +0800

    data.php

--- applications/Workerman.net/data/data.php
+++ applications/Workerman.net/data/data.php
@@ -1,10 +1,10 @@
 <?php 
  return array (
-  'download_workerman_count' => 1946,
-  'download_statistics_count' => 319,
-  'download_thrift_count' => 175,
-  'download_jsonrpc_count' => 224,
-  'download_chat_count' => 2741,
-  'download_todpole_count' => 2338,
-  'download_flappybird_count' => 186,
+  'download_workerman_count' => 2228,
+  'download_statistics_count' => 349,
+  'download_thrift_count' => 197,
+  'download_jsonrpc_count' => 245,
+  'download_chat_count' => 2971,
+  'download_todpole_count' => 2431,
+  'download_flappybird_count' => 232,
 );
