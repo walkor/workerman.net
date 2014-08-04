c43bc50f7197f74375282136de9e5348636936e2
walkor <worker-man@qq.com>
2014-08-04 18:23:01 +0800

    Ya

--- applications/Workerman.net/Views/applications.tpl.php
+++ applications/Workerman.net/Views/applications.tpl.php
@@ -3,59 +3,74 @@
 		相关应用
 	</li>
 </ul>
-<div class="media">
-	<a href="http://dou.xiumeier.com/" target="_blank" class="pull-left"><img src="/img/dou-shou-qi.jpg" class="media-object" alt='斗兽棋游戏' /></a>
-	<div class="media-body">
-		<h4 class="media-heading">
-			<a target="_blank" href="http://dou.xiumeier.com/">斗兽棋</a>&nbsp;&nbsp;<font style="font-size:12px;color:red;">new</font>
-		</h4>
-		<p>在线斗兽棋:斗兽棋是中国一种棋类游戏,80后儿时的玩具,双方有八只棋子，依大小顺序为象、狮、虎、豹、犬、狼、猫、鼠。较大的可吃较小的，同类可以互吃，而鼠则可吃象，象不能吃鼠。动物走一格，前后左右都可以，极具趣味性。<a target="_blank" class="btn" href="http://dou.xiumeier.com/">本应用由 dou.xiumeier.com 提供</a></p>
-	</div>
-</div>
-<div class="media">
-	 <a href="/workerman-flappybird" class="pull-left"><img src="/img/workerman-flappy-bird.png" class="media-object" alt='workerman多人在线 flappy bird 游戏截图' /></a>
-	<div class="media-body">
-		<h4 class="media-heading">
-			<a target="_blank" href="/workerman-flappybird">flappy bird 游戏多人在线版</a>&nbsp;&nbsp;<font style="font-size:12px;color:red;">推荐</font>
-		</h4>
-		<p>使用HTML5+WebSocket+Workerman实时推送技术开发的多人在线版flappy bird，游戏中你可以看到无数玩家同时控制小鸟前赴后继地向前送死的壮烈(搞笑)场面。<a target="_blank" class="btn" href="/workerman-flappybird">详情 »</a></p>
-	</div>
-</div>
-<div class="media">
-	 <a href="/workerman-todpole" class="pull-left"><img src="/img/workerman-todpole.png" class="media-object" alt='PHP 小蝌蚪游戏界面' /></a>
-	<div class="media-body">
-		<h4 class="media-heading">
-			<a target="_blank" href="/workerman-todpole">PHP小蝌蚪实时交互游戏</a>&nbsp;&nbsp;<font style="font-size:12px;color:red;">推荐</font>
-		</h4>
-		<p>使用HTML5+WebSocket+Workerman实时推送技术开发的蝌蚪小游戏，游戏中玩家们可以实时互动，并且可以实时聊天，非常有趣。<a target="_blank" class="btn" href="/workerman-todpole">详情 »</a></p>
-	</div>
-</div>
-<div class="media">
-	 <a href="/workerman-thrift" class="pull-left"><img src="/img/workerman-thrift.png" class="media-object" alt="workerman thrift RPC模块关系示意图"/></a>
-	<div class="media-body">
-		<h4 class="media-heading">
-			<a target="_blank" href="/workerman-thrift">Thrift RPC远程调用框架</a>
-		</h4>
-		 <p>Workerman和Thrift框架相结合，目的是解决异构系统与PHP通信的问题，使你的服务更方便的被其它语言调用。<a target="_blank" class="btn" href="/workerman-thrift">详情 »</a></p>
-	</div>
-</div>
-<div class="media">
-	 <a href="/workerman-chat" class="pull-left"><img src="/img/workerman-chat.png" class="media-object" alt="PHP聊天室示意图"/></a>
-	<div class="media-body">
-		<h4 class="media-heading">
-			<a target="_blank" href="/workerman-chat">聊天室</a>
-		</h4>
-		<p>一个可扩展的基于Websocket协议的聊天室，服务端采用workerman作为Socket服务器框架，客户端支持多浏览器统一接口，支持浏览器原生Websocket同时支持Flash实现的Websocket。<a target="_blank" class="btn" href="/workerman-chat">详情 »</a></p>
-	</div>
-</div>
-<div class="media">
-	 <a href="/workerman-statistics" class="pull-left"><img src="/img/workerman-statistics.png" class="media-object" alt="PHP统计系统示意图" /></a>
-	<div class="media-body">
-		<h4 class="media-heading">
-			<a target="_blank" href="/workerman-statistics">分布式统计系统</a>
-		</h4>
-		<p>基于workerman socket框架开发的一款分布式统计监控系统，纯PHP开发、无需安装Mysql、Nginx、Apache等软件，只要你安装了PHP就行，需要Linux系统哦。<a target="_blank" class="btn" href="/workerman-statistics">详情 »</a></p>
-	</div>
-</div>
+			<div class="media">
+			 <a href="/workerman-todpole" class="pull-left"><img src="/img/workerman-todpole.png" class="media-object" alt='PHP 小蝌蚪游戏界面' /></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					<a target="_blank" href="/workerman-todpole">PHP小蝌蚪实时交互游戏</a>&nbsp;&nbsp;<font style="font-size:12px;color:red;">置顶推荐</font>
+				</h4>
+				<p>使用HTML5+WebSocket+Workerman实时推送技术开发的蝌蚪小游戏，游戏中玩家们可以实时互动，并且可以实时聊天，非常有趣。<a target="_blank" class="btn" href="/workerman-todpole">详情 »</a></p>
+			</div>
+		</div>
+		<div class="media">
+			 <a href="http://www.yuchats.com/" class="pull-left" target="_blank"><img src="/img/app-ya.png" class="media-object" alt='workerman 手机im ya' /></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					<a target="_blank" href="/workerman-todpole">YA</a>&nbsp;&nbsp;<font style="font-size:12px;color:red;">新</font>
+				</h4>
+				<p>Ya！是一款极致简单、快速有效的下一代互联网沟通方式！
+用户登录后通过右划好友头像，向对方传递个通知，而对方收到通知后，也可以通过右划进行反馈。整个沟通过程极致有效，并有无数种使用方法。<br>
+" Ya "（Hi！）<br>
+" Ya Ya "（你好呀！）<br>
+" Ya Ya Ya Ya "（一起吃饭呗！）<br>
+" Ya "（好呀！） <br>
+				<a target="_blank" class="btn" href="http://www.yuchats.com/">本应用由www.yuchats.com提供</a></p>
+			</div>
+		</div>
+		<div class="media">
+			 <a href="http://dou.xiumeier.com/" target="_blank" class="pull-left"><img src="/img/dou-shou-qi.jpg" class="media-object" alt='斗兽棋游戏' /></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					<a target="_blank" href="http://dou.xiumeier.com/">斗兽棋</a>&nbsp;&nbsp;<font style="font-size:12px;color:red;">new</font>
+				</h4>
+				<p>在线斗兽棋:斗兽棋是中国一种棋类游戏,80后儿时的玩具,双方有八只棋子，依大小顺序为象、狮、虎、豹、犬、狼、猫、鼠。较大的可吃较小的，同类可以互吃，而鼠则可吃象，象不能吃鼠。动物走一格，前后左右都可以，极具趣味性。<a target="_blank" class="btn" href="http://dou.xiumeier.com/">本应用由 dou.xiumeier.com 提供</a></p>
+			</div>
+		</div>
+		<div class="media">
+			 <a href="/workerman-flappybird" class="pull-left"><img src="/img/workerman-flappy-bird.png" class="media-object" alt='workerman多人在线 flappy bird 游戏截图' /></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					<a target="_blank" href="/workerman-flappybird">flappy bird 游戏多人在线版</a>&nbsp;&nbsp;<font style="font-size:12px;color:red;">推荐</font>
+				</h4>
+				<p>使用HTML5+WebSocket+Workerman实时推送技术开发的多人在线版flappy bird，游戏中你可以看到无数玩家同时控制小鸟前赴后继地向前送死的壮烈(搞笑)场面。<a target="_blank" class="btn" href="/workerman-flappybird">详情 »</a></p>
+			</div>
+		</div>
+		<div class="media">
+			 <a href="/workerman-thrift" class="pull-left"><img src="/img/workerman-thrift.png" class="media-object" alt="workerman thrift RPC模块关系示意图"/></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					<a target="_blank" href="/workerman-thrift">Thrift RPC远程调用框架</a>
+				</h4>
+				 <p>Workerman和Thrift框架相结合，目的是解决异构系统与PHP通信的问题，使你的服务更方便的被其它语言调用。<a target="_blank" class="btn" href="/workerman-thrift">详情 »</a></p>
+			</div>
+		</div>
+		<div class="media">
+			 <a href="/workerman-chat" class="pull-left"><img src="/img/workerman-chat.png" class="media-object" alt="PHP聊天室示意图"/></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					<a target="_blank" href="/workerman-chat">聊天室</a>
+				</h4>
+				<p>一个可扩展的基于Websocket协议的聊天室，服务端采用workerman作为Socket服务器框架，客户端支持多浏览器统一接口，支持浏览器原生Websocket同时支持Flash实现的Websocket。<a target="_blank" class="btn" href="/workerman-chat">详情 »</a></p>
+			</div>
+		</div>
+		<div class="media">
+			 <a href="/workerman-statistics" class="pull-left"><img src="/img/workerman-statistics.png" class="media-object" alt="PHP统计系统示意图" /></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					<a target="_blank" href="/workerman-statistics">分布式统计系统</a>
+				</h4>
+				<p>基于workerman socket框架开发的一款分布式统计监控系统，纯PHP开发、无需安装Mysql、Nginx、Apache等软件，只要你安装了PHP就行，需要Linux系统哦。<a target="_blank" class="btn" href="/workerman-statistics">详情 »</a></p>
+			</div>
+		</div>
 
 <br><?php global $bd_ad_960_90; echo $bd_ad_960_90;?>
