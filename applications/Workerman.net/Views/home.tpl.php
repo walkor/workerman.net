d72ff49c2bf8a6cb2d3ab888841a4763e9d62fb0
walkor <worker-man@qq.com>
2014-08-04 18:22:22 +0800

    Ya

--- applications/Workerman.net/Views/home.tpl.php
+++ applications/Workerman.net/Views/home.tpl.php
@@ -78,6 +78,30 @@
 			相关应用
 		</h3>
 		<div class="media">
+			 <a href="/workerman-todpole" class="pull-left"><img src="/img/workerman-todpole.png" class="media-object" alt='PHP 小蝌蚪游戏界面' /></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					<a target="_blank" href="/workerman-todpole">PHP小蝌蚪实时交互游戏</a>&nbsp;&nbsp;<font style="font-size:12px;color:red;">置顶推荐</font>
+				</h4>
+				<p>使用HTML5+WebSocket+Workerman实时推送技术开发的蝌蚪小游戏，游戏中玩家们可以实时互动，并且可以实时聊天，非常有趣。<a target="_blank" class="btn" href="/workerman-todpole">详情 »</a></p>
+			</div>
+		</div>
+		<div class="media">
+			 <a href="http://www.yuchats.com/" class="pull-left" target="_blank"><img src="/img/app-ya.png" class="media-object" alt='workerman 手机im ya' /></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					<a target="_blank" href="/workerman-todpole">YA</a>&nbsp;&nbsp;<font style="font-size:12px;color:red;">新</font>
+				</h4>
+				<p>Ya！是一款极致简单、快速有效的下一代互联网沟通方式！
+用户登录后通过右划好友头像，向对方传递个通知，而对方收到通知后，也可以通过右划进行反馈。整个沟通过程极致有效，并有无数种使用方法。<br>
+" Ya "（Hi！）<br>
+" Ya Ya "（你好呀！）<br>
+" Ya Ya Ya Ya "（一起吃饭呗！）<br>
+" Ya "（好呀！） <br>
+				<a target="_blank" class="btn" href="http://www.yuchats.com/">本应用由www.yuchats.com提供</a></p>
+			</div>
+		</div>
+		<div class="media">
 			 <a href="http://dou.xiumeier.com/" target="_blank" class="pull-left"><img src="/img/dou-shou-qi.jpg" class="media-object" alt='斗兽棋游戏' /></a>
 			<div class="media-body">
 				<h4 class="media-heading">
@@ -93,15 +117,6 @@
 					<a target="_blank" href="/workerman-flappybird">flappy bird 游戏多人在线版</a>&nbsp;&nbsp;<font style="font-size:12px;color:red;">推荐</font>
 				</h4>
 				<p>使用HTML5+WebSocket+Workerman实时推送技术开发的多人在线版flappy bird，游戏中你可以看到无数玩家同时控制小鸟前赴后继地向前送死的壮烈(搞笑)场面。<a target="_blank" class="btn" href="/workerman-flappybird">详情 »</a></p>
-			</div>
-		</div>
-		<div class="media">
-			 <a href="/workerman-todpole" class="pull-left"><img src="/img/workerman-todpole.png" class="media-object" alt='PHP 小蝌蚪游戏界面' /></a>
-			<div class="media-body">
-				<h4 class="media-heading">
-					<a target="_blank" href="/workerman-todpole">PHP小蝌蚪实时交互游戏</a>&nbsp;&nbsp;<font style="font-size:12px;color:red;">推荐</font>
-				</h4>
-				<p>使用HTML5+WebSocket+Workerman实时推送技术开发的蝌蚪小游戏，游戏中玩家们可以实时互动，并且可以实时聊天，非常有趣。<a target="_blank" class="btn" href="/workerman-todpole">详情 »</a></p>
 			</div>
 		</div>
 		<div class="media">
