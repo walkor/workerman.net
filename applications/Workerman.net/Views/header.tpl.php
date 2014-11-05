f0c2cca2c8f2c91a72262e4006b8bdaa7ff91b9a
walkor <worker-man@qq.com>
2014-11-05 19:34:56 +0800

    header

--- applications/Workerman.net/Views/header.tpl.php
+++ applications/Workerman.net/Views/header.tpl.php
@@ -72,7 +72,7 @@
 							<a href="http://update.workerman.net" target="_blank">历史版本</a>
 						</li>
 						<li<?php if($html_nav == 'windows')echo ' class="active" ';?>>
-							<a href="/windows" ><img src="/img/windows.png">&nbsp;windows版本</a>
+							<a href="/windows" ><img src="/img/windows.png">&nbsp;windows多线程版本</a>
 						</li>
 					</ul>
 				</div>
