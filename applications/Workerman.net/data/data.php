e4be41599e4f54e8dcee969d1dbb1be97df6c669
walkor <worker-man@qq.com>
2014-11-08 00:02:17 +0800

    不小心up-C了，补回来

--- applications/Workerman.net/data/data.php
+++ applications/Workerman.net/data/data.php
@@ -1,10 +1,10 @@
 <?php 
  return array (
-  'download_workerman_count' => 2969,
-  'download_statistics_count' => 435,
-  'download_thrift_count' => 273,
-  'download_jsonrpc_count' => 319,
-  'download_chat_count' => 3480,
-  'download_todpole_count' => 2640,
-  'download_flappybird_count' => 309,
+  'download_workerman_count' => 4969,
+  'download_statistics_count' => 1435,
+  'download_thrift_count' => 1273,
+  'download_jsonrpc_count' => 1319,
+  'download_chat_count' => 5480,
+  'download_todpole_count' => 3640,
+  'download_flappybird_count' => 1309,
 );
