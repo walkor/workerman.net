11827e29b2b44062da59652dcb1e531e10f27de9
walkor <worker-man@qq.com>
2014-12-30 22:15:06 +0800

    add data.php

--- applications/Workerman.net/data/data.php
+++ applications/Workerman.net/data/data.php
@@ -1,11 +1,11 @@
 <?php 
  return array (
-  'download_workerman_count' => 6340,
-  'download_statistics_count' => 1559,
-  'download_thrift_count' => 1362,
-  'download_jsonrpc_count' => 1478,
-  'download_chat_count' => 6314,
-  'download_todpole_count' => 3927,
-  'download_flappybird_count' => 1405,
-  'download_workermanwin_count' => 331,
+  'download_workerman_count' => 6390,
+  'download_statistics_count' => 1599,
+  'download_thrift_count' => 1392,
+  'download_jsonrpc_count' => 1498,
+  'download_chat_count' => 6394,
+  'download_todpole_count' => 3997,
+  'download_flappybird_count' => 1495,
+  'download_workermanwin_count' => 371,
 );
