d0c973c311876882439f0172ef6a515b2b5838a2
walkor <worker-man@qq.com>
2015-01-21 21:20:18 +0800

    data save

--- applications/Workerman.net/data/data.php
+++ applications/Workerman.net/data/data.php
@@ -1,11 +1,11 @@
 <?php 
  return array (
-  'download_workerman_count' => 6390,
-  'download_statistics_count' => 1599,
-  'download_thrift_count' => 1392,
-  'download_jsonrpc_count' => 1498,
-  'download_chat_count' => 6394,
-  'download_todpole_count' => 3997,
-  'download_flappybird_count' => 1495,
-  'download_workermanwin_count' => 371,
+  'download_workerman_count' => 7249,
+  'download_statistics_count' => 1651,
+  'download_thrift_count' => 1440,
+  'download_jsonrpc_count' => 1559,
+  'download_chat_count' => 6828,
+  'download_todpole_count' => 4121,
+  'download_flappybird_count' => 1556,
+  'download_workermanwin_count' => 571,
 );
