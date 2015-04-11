648a2318f950df9c99245a3335e5aaf6528179ba
walkor <worker-man@qq.com>
2015-04-11 23:36:23 +0800

    update link

--- applications/Workerman.net/Views/header.tpl.php
+++ applications/Workerman.net/Views/header.tpl.php
@@ -69,7 +69,7 @@
 							<a href="http://wenda.workerman.net" target="_blank">问答</a>
 						</li>
 						<li<?php if($html_nav == 'update')echo ' class="active" ';?>>
-							<a href="http://update.workerman.net" target="_blank">更新日志</a>
+							<a href="https://github.com/walkor/workerman/releases" target="_blank">更新日志</a>
 						</li>
 					</ul>
 				</div>
