5e50972a579e1a7e9ef15e24d55530d659ea0163
walkor <walkor@workerman.net>
2015-06-12 21:07:44 +0800

    data

--- applications/Workerman.net/data/data.php
+++ applications/Workerman.net/data/data.php
@@ -1,11 +1,11 @@
 <?php 
  return array (
-  'download_workerman_count' => 8759,
-  'download_statistics_count' => 1816,
-  'download_thrift_count' => 1554,
-  'download_jsonrpc_count' => 1722,
-  'download_chat_count' => 8088,
-  'download_todpole_count' => 4368,
-  'download_flappybird_count' => 1687,
-  'download_workermanwin_count' => 852,
+  'download_workerman_count' => 12759,
+  'download_statistics_count' => 2716,
+  'download_thrift_count' => 2254,
+  'download_jsonrpc_count' => 2522,
+  'download_chat_count' => 12088,
+  'download_todpole_count' => 6368,
+  'download_flappybird_count' => 2487,
+  'download_workermanwin_count' => 1252,
 );
