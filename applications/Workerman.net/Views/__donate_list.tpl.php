9810d2fb6693f1b05405917493e401d7cb6f95ff
walkor <worker-man@qq.com>
2015-07-20 23:02:22 +0800

    update donate

--- applications/Workerman.net/Views/__donate_list.tpl.php
+++ applications/Workerman.net/Views/__donate_list.tpl.php
@@ -1,8 +1,10 @@
 <table class="table">
 	<tbody>
+		<tr class="active"><td>300.0￥</td><td>匿名</td><td>2015-07-20</td><td>支付宝</td></tr>
+		<tr class="active"><td>100.0￥</td><td>金汉江</td><td>2015-07-20</td><td>支付宝</td></tr>
 		<tr class="active"><td>100.0￥</td><td>林涛</td><td>2015-07-20</td><td>支付宝</td></tr>
 		<tr class="active"><td>100.0￥</td><td>王飞</td><td>2015-07-20</td><td>支付宝</td></tr>
-                <tr class="active"><td>10.0￥</td><td>洪世辉</td><td>2015-07-14</td><td>支付宝</td></tr>
+		<tr class="active"><td>10.0￥</td><td>洪世辉</td><td>2015-07-14</td><td>支付宝</td></tr>
 		<tr class="active">
 			<td>
 				10.0￥
