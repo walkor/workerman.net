4f01fa69f6b0324f2d4444fe176905a47a4edbb8
walkor <walkor@workerman.net>
2015-07-21 14:22:37 +0800

    fix

--- applications/Workerman.net/Views/workerman.tpl.php
+++ applications/Workerman.net/Views/workerman.tpl.php
@@ -97,13 +97,13 @@ use Workerman\Worker;
 require_once \'./Workerman/Autoloader.php\';
 
 // 创建一个Worker监听2346端口，使用websocket协议通讯
-\$ws_worker = new Worker("websocket://0.0.0.0:2346");
+$ws_worker = new Worker("websocket://0.0.0.0:2346");
 
 // 启动4个进程对外提供服务
-\$ws_worker->count = 4;
+$ws_worker->count = 4;
 
 // 当收到客户端发来的数据后返回hello $data给客户端
-\$ws_worker->onMessage = function($connection, $data)
+$ws_worker->onMessage = function($connection, $data)
 {
     // 向客户端发送hello $data
     $connection->send(\'hello \' . $data);
