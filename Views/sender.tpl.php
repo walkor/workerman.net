db1392be87fbadc4b6d439271d9eed8bde4b13e5
walkor <worker-man@qq.com>
2015-10-15 23:05:01 +0800

    update web-msg-sender

--- Views/sender.tpl.php
+++ Views/sender.tpl.php
@@ -12,19 +12,26 @@
 				WEB消息推送框架
 			</h2>
 			<p class="f16">
-				web-msg-sender 是一款web推送框架，采用服务器推送技术，通讯基于WebSocket协议，如果浏览器不支持WebSocket则自动转用flash websocket推送。
-				通过后台推送消息，消息可以即时推送到客户端，可以做到针对某个用户推送数据（需要开发）。服务端采用Workerman，支持Epoll，能够轻松支持上万并发连接，非轮询，性能极高。
+				web-msg-sender是一款web推送框架，采用PHPSocket.IO开发，基于WebSocket长连接通讯，如果浏览器不支持WebSocket则自动转用comet技术推送。
+				通过后台推送消息，消息可以即时推送到客户端，非轮询，实时性非常好，性能很高。
 			</p>
-			<p class="f16">
-				web-msg-sender采用经典的gateway worker进程模型。gateway只负责网络IO，负责接受网页上发来的websocket连接，维持这些连接。Gateway进程是全异步非阻塞的，每个gateway进程都可以同时接受上万客户端连接。
-				workers采用的是PHP开发者所熟悉的同步模型，开发者选择性的实现onConnect、onMessage、onClose回调皆可轻松扩展功能，开发扩展非常容易。
+			<div class="f16">
+			<b>特点：</b><br>
+			<ul>
+			<li>多浏览器支持</li>
+			<li>支持针对单个用户推送消息</li>
+			<li>支持向所有用户推送消息</li>
+			<li>长连接推送（websocket或者comet），消息即时到达</li>
+			<li>支持在线用户数实时统计展示</li>
+			<li>支持在线页面数实时统计展示</li>
+			<li>支持跨域推送</li>
+			</ul>
+			</div>
+			<p class="f16">接收消息页面：<a rel="nofollow" href="http://www.workerman.net:2123/">http://www.workerman.net:2123/</a></p>
+			<p class="f16">推送接口url：<a rel="nofollow" href="http://www.workerman.net:2121/?type=publish&to=&content=content">http://www.workerman.net:2121/?type=publish&to=uid&content=content</a><br>
+其中to为接收消息的uid，如果传空则向所有人推送消息
+content 为消息内容
 			</p>
-			<p class="f16">
-				gateway worker之间是无状态的，gateway和workers可以分别部署在不同的物理机上，实现分布式部署，扩容和升级都非常方便。
-				业务接入只需要引入一段js即可实现推送，能做到与现有业务解耦，整合维护很简单。
-			</p>
-			<p class="f16">后台发消息的Demo页面：<a href="http://workerman.net:3333/" target="_blank" rel="nofollow">http://workerman.net:3333/</a></p>
-			<p class="f16">用户接受消息的Demo页面：<a href="http://workerman.net/web-msg-sender.html" target="_blank" rel="nofollow">http://workerman.net/web-msg-sender.html 可以多开几个</a></p>
 			<div class="bd-ad"><?php global $bd_ad_468_60; echo $bd_ad_468_60;?></div>
 		</div>
 		<div class="col-md-5 column text-center">
@@ -61,12 +68,34 @@
 			<p>1、<a href="/download/senderzip" target="_blank">下载</a>web-msg-sender，并解压缩到任意目录</p>
 			<p>2、启动workerman<code>php start.php start -d</code>如下图</p>
 			<img src="/img/sender-start.png" alt="WEB推送启动示意图"/>
-			<p>3、浏览器访问端口http://ip:3333或者http://域名:3333，例如<a href="http://workerman.net:3333/">http://workerman.net:3333</a>如图：</p>
-			<img src="/img/sender.png" alt="WEB推送使用界面"/>
-			<h3>Windows系统安装教程</h3>
-			<h4>参见源码中README.md</h4>
+			<p>3、浏览器访问端口http://ip:2123或者http://域名:2123，例如<a href="http://workerman.net:2123/">http://workerman.net:2123</a>如图：</p>
+			<img src="/img/web-msg-sender-demo.png" alt="WEB推送使用界面"/>
 			<br>
-			<h3>说明</h3>
-			<p>本框架逻辑非常简单，业务逻辑都在文件./Applications/Sender/Event.php中，开发者可以随意修改，例如针对某个用户推送消息</p>
+			<h3>测试：</h3>
+			<p>支持跨域推送，开发者可以不用建立服务端，直接使用线上的推送服务测试，只要引入js文件并设置下端口及回调即可，例如在任意站点中加入如下代码即可收到消息并统计数据：</p>
+			<pre>
+<script src='http//cdn.bootcss.com/socket.io/1.3.7/socket.io.js'></script>
+<script>
+    // 连接服务端
+    var socket = io('http://workerman.net:2120');
+    // uid可以是自己网站的用户id，以便针对uid推送以及统计在线人数
+    uid = 123;
+    // socket连接后以uid登录
+    socket.on('connect', function(){
+    	socket.emit('login', uid);
+    });
+    // 后端推送来消息时
+    socket.on('new_msg', function(msg){
+        console.log("收到消息："+msg);
+    }
+    // 后端推送来在线数据时
+    socket.on('update_online_count', function(online_stat){
+        console.log(online_stat);
+    }
+</script>
+			</pre>
+			<p>在站点中加入以上代码后便可以通过访问或者后端调用
+推送接口url：<a rel="nofollow" href="http://www.workerman.net:2121/?type=publish&to=123&content=这是消息内容">http://www.workerman.net:2121/?type=publish&to=123&content=这是消息内容</a>
+推送数据了</p>
 		</div>
 	</div>
