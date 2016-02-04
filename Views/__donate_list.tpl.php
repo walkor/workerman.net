011f23b0015c133a0ae2f6a87d71991a43c0b6f5
walkor <walkor@users.noreply.github.com>
2016-02-04 22:23:42 +0800

    Update __donate_list.tpl.php

--- Views/__donate_list.tpl.php
+++ Views/__donate_list.tpl.php
@@ -1,5 +1,7 @@
 <table class="table">
 	<tbody>
+		<tr class="active"><td>20.0￥</td><td>朱靖</td><td>2016-02-04</td><td>支付宝</td></tr>
+		<tr class="active"><td>1.0￥</td><td>凉城</td><td>2016-02-04</td><td>微信</td></tr>
 		<tr class="active"><td>10.0￥</td><td>完美生活</td><td>2016-02-02</td><td>微信</td></tr>
 		<tr class="active"><td>0.70￥</td><td>徐一洋</td><td>2016-02-01</td><td>支付宝</td></tr>
 		<tr class="active"><td>10.0￥</td><td>孔德建-H5游戏开发</td><td>2016-01-31</td><td>微信</td></tr>
