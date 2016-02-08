ca1cec9360498d4ce543a6111e03b20cad0f6a79
walkor <worker-man@qq.com>
2016-02-08 10:33:11 +0800

    qun2 2000

--- Views/footer.tpl.php
+++ Views/footer.tpl.php
@@ -46,9 +46,9 @@
 						<ul class="sub">
 						<li>邮箱：walkor@workerman.net</li>
 						<li>
-						QQ群1：257046084(已满)<br>
-						QQ群2：137410824(已满)<br>
-						QQ群3：527941005
+						QQ群1：257046084(2000人已满)<br>
+						QQ群2：137410824(2000人)<br>
+						QQ群3：527941005(500人)<br>
 						<a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=beb88dc6303c25e893ac433260e7c682b27efd3935eeb060d7ede5b213ff13fc"><img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="workerman官方群3" title="workerman官方群3"></a>
 						</li>
 						</ul>
