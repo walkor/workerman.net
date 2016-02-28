fad0fe180a86f31600bcb667ccbe245a3fc7889f
walkor <worker-man@qq.com>
2016-02-28 22:40:53 +0800

    update

--- Views/last_update.tpl.php
+++ Views/last_update.tpl.php
@@ -1,4 +1,7 @@
 <h3>2016-02-28</h3>
-<div>workerman3.3.0版本发布，新增<a href = "http://doc3.workerman.net/appendices/about-frame.html">frame协议</a>。</div>
-<div>发布全局变量共享组件<a href="http://doc3.workerman.net/component/global-data.html">GlobalData</a>(需要workerman>=3.3.0)，用于进程间/服务器间变量共享。</div>
-<div>升级进程间/服务器间通讯组件<a href="http://doc3.workerman.net/component/channel.html">Channel</a>(需要workerman>=3.3.0)，使之支持二进制数据通讯。</div>
+<hr>
+<ul>
+<li>workerman3.3.0版本发布，新增<a href = "http://doc3.workerman.net/appendices/about-frame.html">frame协议</a>。</li>
+<li>发布全局变量共享组件<a href="http://doc3.workerman.net/component/global-data.html">GlobalData</a>(需要workerman>=3.3.0)，用于进程间/服务器间变量共享。</li>
+<li>升级进程间/服务器间通讯组件<a href="http://doc3.workerman.net/component/channel.html">Channel</a>(需要workerman>=3.3.0)，使之支持二进制数据通讯。</li>
+</ul>
