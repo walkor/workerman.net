2f53c69a63279aeb5831fbd81ac9abafc9566498
walkor <worker-man@qq.com>
2016-02-28 22:37:07 +0800

    fix

--- Modules/last_update.php
+++ Modules/last_update.php
@@ -1,13 +1,13 @@
 <?php
 namespace WorkerMan\Modules;
 
-function applications()
+function last_update()
 {
     $html_title = 'workerman最近更新';
-    $html_nav = 'last_change';
+    $html_nav = 'last_update';
     $html_keywords = 'workerman 最近更新';
     include NET_ROOT . '/Views/header.tpl.php';
-    include NET_ROOT . '/Views/last_change.tpl.php';
+    include NET_ROOT . '/Views/last_update.tpl.php';
     include NET_ROOT . '/Views/footer.tpl.php';
 }
 
