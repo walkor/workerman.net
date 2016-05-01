8f4721633a543adc819556bffab4c9796bcaa0eb
walkor <worker-man@qq.com>
2016-05-01 21:34:10 +0800

    update

--- Views/donate.tpl.php
+++ Views/donate.tpl.php
@@ -36,7 +36,9 @@
 <br>
 感谢您对Workerman的支持!
 <br><br>
-<h3><a href="/donate_list">查看捐赠列表</a></h3>
+<h3>捐赠列表</h3>
+        <?php include __DIR__ . '/__donate_list.tpl.php';?>
+	<br><br>
 	</div>
 	<div class="col-md-3 column">
 	
