06649fbe55a38cc8232abe4db0a927c140e8b946
walkor <worker-man@qq.com>
2016-05-02 17:50:53 +0800

    update

--- Views/install.tpl.php
+++ Views/install.tpl.php
@@ -13,7 +13,7 @@
 	<h5>1、命令行运行<code>yum install php-cli php-process  git gcc  php-devel php-pear libevent-devel</code></h5>
 	<h5>2、命令行运行<code>pecl install channel://pecl.php.net/libevent-0.1.0</code> 提示libevent installation [autodetect]: 时按回车</h5>
 	<h5>3、命令行运行<code>echo extension=libevent.so > /etc/php.d/libevent.ini</code></h5>
-	<h5>4、命令行运行<code>git clone https://github.com/walkor/workerman</code></h5>
+	<h5>4、命令行运行<code>git clone https://github.com/walkor/Workerman</code></h5>
 	<h5>5、参考<a href="http://doc3.workerman.net/getting-started/simple-example.html" target="_blank">手册</a>写例子运行或者从主页下载<a href="/applications">demo</a>运行</h5>
 	<br>
 	<h3>debian/ubuntu系统安装教程</h3>
@@ -21,7 +21,7 @@
 	<h5>1、命令行运行<code>apt-get install php5-cli git gcc php-pear php5-dev libevent-dev</code></h5>
 	<h5>2、命令行运行<code>pecl install channel://pecl.php.net/libevent-0.1.0</code> 提示libevent installation [autodetect]: 时按回车</h5>
 	<h5>3、命令行运行(需要切换到root用户)<code>echo extension=libevent.so > /etc/php5/cli/conf.d/libevent.ini</code></h5>
-	<h5>4、命令行运行<code>git clone https://github.com/walkor/workerman</code></h5>
+	<h5>4、命令行运行<code>git clone https://github.com/walkor/Workerman</code></h5>
 	<h5>5、参考<a href="http://doc3.workerman.net/getting-started/simple-example.html" target="_blank">手册</a>写例子运行或者从主页下载<a href="/applications">demo</a>运行</h5>
 	<br>
 	<h3>以上命令说明</h3>
