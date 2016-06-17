87b60502f8860688b9491f05d7378be778e46f36
walkor <walkor@users.noreply.github.com>
2016-06-17 08:49:05 +0800

    Update home.tpl.php

--- Views/home.tpl.php
+++ Views/home.tpl.php
@@ -485,7 +485,7 @@
 			<li>
 				<a href="http://wenda.workerman.net" class="c2"><b>去社区提问</b></a>
 			</li>
-			<li><b class="c2">发邮件至&nbsp;walkor@workerman.net</b></li>
+			
 			<li>
 				免费QQ群：137410824
 				<a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=a7255bef60d796f72b1d7f16b2f6a745fd74ab1d15d6c9de6e031f3978817c3c"><img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="workerman官方群2" title="workerman官方群2"></a>
