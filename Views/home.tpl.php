17c217e26862e08a5571d11ba0105e8702fad172
walkor <worker-man@qq.com>
2016-07-13 23:30:18 +0800

    update

--- Views/home.tpl.php
+++ Views/home.tpl.php
@@ -12,7 +12,8 @@
 			<div class="col-md-7 column">
 				<table><tbody><tr><td><iframe src="http://ghbtns.com/github-btn.html?user=walkor&amp;repo=workerman&amp;type=watch&amp;count=true&amp;size=large" height="30" width="170" frameborder="0" scrolling="0" style="width:150px; height: 30px;" allowtransparency="true"></iframe></td><td><iframe src="http://ghbtns.com/github-btn.html?user=walkor&amp;repo=workerman&amp;type=fork&amp;count=true&amp;size=large" height="30" width="170" frameborder="0" scrolling="0" style="width:150px; height: 30px;" allowtransparency="true"></iframe></td></tr></tbody></table>
 				<a class="btn btn-default btn-large" href="https://github.com/walkor/workerman" target="_blank" rel="nofollow"><b>Fork on Github</b></a>&nbsp;
-				<a class="btn btn-default btn-large" href="/download/workermanzip"><b>下载ZIP压缩文件</b></a> <font class='f11 gray'>下载<?php echo $workerman_download_count;?>次</font>
+				<a class="btn btn-default btn-large" href="/download/workermanzip"><b>Linux版下载</b></a>&nbsp;
+				<a class="btn btn-default btn-large" href="/download/workermanwinzip"><b>Win版下载</b></a>
 				</div>
 				<div class="col-md-5 column">
 					<div class="Q">
