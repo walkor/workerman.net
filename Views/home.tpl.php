c633c32361e7a6514d88f267a23bf2f82af9eaff
李亮 <walkor@users.noreply.github.com>
2016-07-30 23:20:08 +0800

    Update home.tpl.php

--- Views/home.tpl.php
+++ Views/home.tpl.php
@@ -17,10 +17,7 @@
 				</div>
 				<div class="col-md-5 column">
 					<div class="Q">
-						<b>免费Q群：137410824</b> 	<a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=a7255bef60d796f72b1d7f16b2f6a745fd74ab1d15d6c9de6e031f3978817c3c"><img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="workerman官方群2" title="workerman官方群2"></a>
-					</div>
-					<div class="Q">
-						<b>&nbsp;VIPQ群：544584915</b> <a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=48fac857de63cfc2847595e14e168040a1416fa5f82ab6f72fdd778e3c67a8fe"><img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="Workerman 官方收费群1" title="Workerman 官方收费群1"></a>
+						<b>技术交流群：137410824</b> 	<a target="_blank" href="http://shang.qq.com/wpa/qunwpa?idkey=a7255bef60d796f72b1d7f16b2f6a745fd74ab1d15d6c9de6e031f3978817c3c"><img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="workerman官方群2" title="workerman官方群2"></a>
 					</div>
 				</div>
 		</div>
