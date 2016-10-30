45d379bb77bb7905f40b0b1d3664d10cf710ed56
walkor <worker-man@qq.com>
2016-10-30 22:00:25 +0800

    up

--- Views/download_list.tpl.php
+++ Views/download_list.tpl.php
@@ -84,7 +84,7 @@
         				</h4>
     					<p></p><table><tbody><tr><td><iframe src="http://ghbtns.com/github-btn.html?user=walkor&amp;repo=workerman-todpole&amp;type=watch&amp;count=true&amp;size=large" height="30" width="170" frameborder="0" scrolling="0" style="width:150px; height: 30px;" allowtransparency="true"></iframe></td><td><iframe src="http://ghbtns.com/github-btn.html?user=walkor&amp;repo=workerman-todpole&amp;type=fork&amp;count=true&amp;size=large" height="30" width="170" frameborder="0" scrolling="0" style="width:150px; height: 30px;" allowtransparency="true"></iframe></td></tr></tbody></table><p></p>
     					<a class="btn btn-default btn-large" href="https://github.com/walkor/workerman-todpole" target="_blank" rel="nofollow"><b>Fork on Github</b></a>&nbsp;
-    					<a class="btn btn-default btn-large" href="/download/todpolezip"><b>下载ZIP压缩文件</b></a> 
+    					<a class="btn btn-default btn-large" href="/download/workerman-todpole.zip"><b>下载ZIP压缩文件</b></a> 
     					<br><br>
     					<h4>Windows版本下载</h4>
 						<a class="btn btn-default btn-large" href="https://github.com/walkor/workerman-todpole-for-win/archive/master.zip"><b>下载Win版本ZIP压缩文件</b> </a> 
