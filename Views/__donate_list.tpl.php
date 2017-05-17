876c64cac20d6533e800ce70276c28b5d7e6fd76
walkor <walkor@workerman.net>
2017-05-17 19:08:18 +0800

    auto save

--- Views/__donate_list.tpl.php
+++ Views/__donate_list.tpl.php
@@ -1,5 +1,6 @@
 <table class="table">
-	<tbody> 
+	<tbody>
+		<tr class="active"><td>0.01¥</td><td>匿名</td><td>2017-05-16</td><td>微信</td></tr> 
 		<tr class="active"><td>99.99¥</td><td>袁玉</td><td>2017-05-16</td><td>支付宝</td></tr>
 		<tr class="active"><td>0.01¥</td><td>匿名</td><td>2017-05-16</td><td>微信</td></tr>
 		<tr class="active"><td>23.0¥</td><td>浑身难受</td><td>2017-05-11</td><td>QQ钱包</td></tr>
