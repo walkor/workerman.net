8a320b89739b8e860f58eac4fa11374263acecc5
walkor <walkor@users.noreply.github.com>
2017-09-08 18:23:59 +0800

    Update applications.tpl.php

--- Views/applications.tpl.php
+++ Views/applications.tpl.php
@@ -29,6 +29,18 @@
 				</p>
 			</div>
 		</div>
+
+                <div class="media">
+			 <a href="https://www.wolive.cc/" class="pull-left" target="_blank"><img src="/img/wolive.png" class="media-object" alt="在线客服系统"/></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					<a target="_blank" href="https://www.wolive.cc" target="_blank">WoLive在线客服系统</a>
+				</h4>
+				<p>Workerman开发的客服系统，任何站点引入一段js即可接入，接入成本极低。支持web站点和移动站点，支持表情、传图、传文件、粘贴截图、离线消息、客服管理、留言、历史记录、会话统计，支持多站点，支持分布式部署、支持万人在线。<br>
+				<a class="btn" target="_blank" href="https://www.wolive.cc/#contact" rel="nofollow">在线客服系统源码</a>
+				</p>
+			</div>
+		</div>
 		
 		<div class="media">
 			 <a href="http://kf.workerman.net/" class="pull-left" target="_blank"><img src="/img/laylive.png" class="media-object" alt="PHP客服系统"/></a>
