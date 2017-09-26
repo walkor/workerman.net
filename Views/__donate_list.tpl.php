5a27160fdd6a8be6e7413da7fe0bd51edd0b6a49
walkor <walkor@users.noreply.github.com>
2017-09-26 14:56:52 +0800

    Update __donate_list.tpl.php

--- Views/__donate_list.tpl.php
+++ Views/__donate_list.tpl.php
@@ -1,5 +1,6 @@
 <table class="table">
 	<tbody>
+		<tr class="active"><td>1¥</td><td>匿名</td><td>2017-09-26</td><td>微信</td></tr>
 		<tr class="active"><td>5¥</td><td>匿名</td><td>2017-09-23</td><td>微信</td></tr>
 		<tr class="active"><td>5¥</td><td>匿名</td><td>2017-09-23</td><td>微信</td></tr>
 		<tr class="active"><td>300¥</td><td>Udie</td><td>2017-09-22</td><td>QQ钱包</td></tr>
