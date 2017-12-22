a9ce1c55dd3b069cf3b32cf8b719a032a85d2fda
walkor <walkor@users.noreply.github.com>
2017-12-22 20:57:02 +0800

    Update home.tpl.php

--- Views/home.tpl.php
+++ Views/home.tpl.php
@@ -17,8 +17,8 @@
 				</div>
 				<div class="col-md-5 column">
 					<div class="Q">
-						<b>技术交流群:172789145</b>
-						<a target="_blank" href="//shang.qq.com/wpa/qunwpa?idkey=c7f59a6c694660e11c5e2bfea392e74bac0f70ec83ce74eaa7ed59917d7a2878"><img border="0" src="/img/add_to_qq_group.png" alt="workerman官方群4" title="workerman官方群4"></a>
+						<b>技术交流群:605328640</b>
+						<a target="_blank" href="//shang.qq.com/wpa/qunwpa?idkey=771db3370f12cf4563b36ef3e36c81655b42f7fcfdc9f289d045c1aea0c32a4f"><img border="0" src="/img/add_to_qq_group.png" alt="workerman官方5群" title="workerman官方5群"></a>
 					</div>
 				</div>
 		</div>
