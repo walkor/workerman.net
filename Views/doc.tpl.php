e22c7af25100e2e8ce2414203d4cae9aa1d07957
walkor <walkor@users.noreply.github.com>
2018-05-15 15:46:25 +0800

    Update doc.tpl.php

--- Views/doc.tpl.php
+++ Views/doc.tpl.php
@@ -9,7 +9,7 @@
 					<a target="_blank" href="http://doc.workerman.net/">Workerman3.x版本手册</a>
 				</h4>
 				<p>Workerman是一个高性能的socket服务器通讯框架，用于快速开发各种网络应用，包括tcp的、udp的、长连接、短连接应用。
-				<br><a target="_blank" class="btn" href="http://doc.workerman.net/">在线手册 »</a> <a href="http://doc3.workerman.net/" target="_blank" rel="nofollow" class="btn">手册备用地址 »</a> <a target="_blank" rel="nofollow" class="btn" href="http://www.kancloud.cn/walkor/workerman">离线手册 »</a>
+				<br><a target="_blank" class="btn" href="http://doc.workerman.net/">在线手册 »</a> <a target="_blank" rel="nofollow" class="btn" href="http://www.kancloud.cn/walkor/workerman">离线手册 »</a>
 				</p>
 			</div>
 		</div>
@@ -24,10 +24,8 @@
 				GatewayWorker分为Gateway进程和Worker进程，天然支持分布式部署，能够支持庞大的连接数（百万甚至千万连接级别的应用）。<br>
 				可用于开发IM聊天应用、移动通讯、游戏后台、物联网、智能家居后台等等。<br>
 				<br>
-				<a class="btn" target="_blank" href="http://doc2.workerman.net/">GatewayWorker2.0手册</a>
-				<a href="http://www.workerman.net/gatewaydoc/" target="_blank" rel="nofollow" class="btn">手册备用地址 »</a>
+				<a class="btn" target="_blank" href="http://doc2.workerman.net/">GatewayWorker手册</a>
 				<a target="_blank" rel="nofollow" class="btn" href="https://www.kancloud.cn/walkor/gateway-worker">离线手册 »</a>
-					
 				</p>
 			</div>
 		</div>
