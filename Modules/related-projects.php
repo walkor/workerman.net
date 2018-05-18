801dc64c74e8cbde9d7766e1c38bc873f16d8cd4
walkor <walkor@users.noreply.github.com>
2018-05-18 11:53:33 +0800

    Update related-projects.php

--- Modules/related-projects.php
+++ Modules/related-projects.php
@@ -6,6 +6,6 @@ function related_projects()
     $html_nav = 'related_projects';
     $html_keywords = 'workerman 第三方项目';
     include NET_ROOT . '/Views/header.tpl.php';
-    include NET_ROOT . '/Views/related_projects.tpl.php';
+    include NET_ROOT . '/Views/related-projects.tpl.php';
     include NET_ROOT . '/Views/footer.tpl.php';
 }
