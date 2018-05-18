37a297837d2859b33782e2a330be2b8848c3247a
walkor <walkor@users.noreply.github.com>
2018-05-18 11:47:06 +0800

    Create related-projects.tpl.php

---
+++ Views/related-projects.tpl.php
@@ -0,0 +1,115 @@
+<h2>workerman相关第三方项目</h2>
+<h3>thinkworker</h3>
+    基于Workerman的高性能网站/API开发框架
+    项目主页：<a href="http://thinkworker.cn/" target="_blank" rel="nofollow">http://thinkworker.cn/</a>
+    项目文档：<a href="http://docs.thinkworker.cn/" target="_blank" rel="nofollow">http://docs.thinkworker.cn/</a>
+<hr>
+<h3>think-worker</h3>
+    ThinkPHP官方开发的ThinkPHP5的workerman扩展
+    项目主页：<a href="https://github.com/top-think/think-worker" target="_blank" rel="nofollow">https://github.com/top-think/think-worker</a>
+    项目文档：<a href="https://www.kancloud.cn/manual/thinkphp5/235128" target="_blank" rel="nofollow">https://www.kancloud.cn/manual/thinkphp5/235128</a>
+<hr>
+<h3>SlightPHP</h3>
+    SlightPHP 高效的PHP敏捷开发框架
+    项目主页：<a href="https://github.com/hetao29/slightphp" target="_blank" rel="nofollow">https://github.com/hetao29/slightphp</a>
+    项目文档：<a href="https://github.com/hetao29/slightphp" target="_blank" rel="nofollow">https://github.com/hetao29/slightphp</a>
+<hr>
+<h3>hide2/api</h3>
+    基于Workerman的高性能API服务
+    项目主页：<a href="https://github.com/hide2/api" target="_blank" rel="nofollow">https://github.com/hide2/api</a>
+    项目文档：<a href="https://github.com/hide2/api" target="_blank" rel="nofollow">https://github.com/hide2/api</a>
+<hr>
+<h3>think-workerman</h3>
+    一个基于workerman，语法类似thinkphp5开发模式的简易异步开发框架，纯composer包+常用类组成
+    项目主页：<a href="https://github.com/Zsoner/think-workerman" target="_blank" rel="nofollow">https://github.com/Zsoner/think-workerman</a>
+    项目文档：<a href="https://github.com/Zsoner/think-workerman" target="_blank" rel="nofollow">https://github.com/Zsoner/think-workerman</a>
+<hr>
+<h3>webworker</h3>
+    基于Workerman实现的自带http server的web开发框架
+    项目主页：<a href="http://ask.webworker.xtgxiso.com/" target="_blank" rel="nofollow">http://ask.webworker.xtgxiso.com/</a>
+    项目文档：<a href="http://doc.webworker.xtgxiso.com/" target="_blank" rel="nofollow">http://doc.webworker.xtgxiso.com/</a>
+<hr>
+<h3>Beanbun</h3>
+    Beanbun 是用 PHP 编写的多进程网络爬虫框架，具有良好的开放性、高可扩展性，基于 Workerman。
+    项目主页：<a href="http://www.beanbun.org" target="_blank" rel="nofollow">http://www.beanbun.org</a>
+    项目文档：<a href="http://www.beanbun.org" target="_blank" rel="nofollow">http://www.beanbun.org</a>
+<hr>
+
+<h3>Workerman_cor_ape</h3>
+    Workerman_cor_ape是php框架 Workerman 的强化版，在不影响任何使用方式，稳定性，性能前提下，增加了异步任务组件。
+    项目主页：<a href="https://github.com/zyfei/workerman_cor_ape" target="_blank" rel="nofollow">https://github.com/zyfei/workerman_cor_ape</a>
+    项目文档：<a href="https://github.com/zyfei/workerman_cor_ape" target="_blank" rel="nofollow">https://github.com/zyfei/workerman_cor_ape</a>
+<hr>
+
+<h3>WorkermanYii2</h3>
+    对于熟悉使用workerman和yii2 ActiveRecod的人来说，在workerman中使用ActiveRecord是一个问题 这个项目主要解决了在workerman中使用yii2中的ActiveRecord的问题
+    项目主页：<a href="https://github.com/victorruan/WorkermanYii2" target="_blank" rel="nofollow">https://github.com/victorruan/WorkermanYii2</a>
+    项目文档：<a href="https://github.com/victorruan/WorkermanYii2" target="_blank" rel="nofollow">https://github.com/victorruan/WorkermanYii2</a>
+<hr>
+<h3>WorkerA</h3>
+    a http framework for workerman
+    项目主页：<a href="https://github.com/wazsmwazsm/WorkerA" target="_blank" rel="nofollow">https://github.com/wazsmwazsm/WorkerA</a>
+    项目文档：<a href="https://github.com/wazsmwazsm/WorkerA/blob/master/README_CN.md" target="_blank" rel="nofollow">https://github.com/wazsmwazsm/WorkerA/blob/master/README_CN.md</a>
+<hr>
+<h3>Workerman-Amp</h3>
+    本项目用于将Amp的event-loop应用于Workerman，从而可以在Workerman中使用基于Amp的高性能组件，例如异步MySQL，异步Redis，异步HTTP客户端等。
+    项目主页：<a href="https://github.com/CismonX/Workerman-Amp" target="_blank" rel="nofollow">https://github.com/CismonX/Workerman-Amp</a>
+    项目文档：<a href="https://github.com/CismonX/Workerman-Amp" target="_blank" rel="nofollow">https://github.com/CismonX/Workerman-Amp</a>
+<hr>
+<h3>socket-service</h3>
+    基于workerman-chat的GatewayWorker框架开发的一款高性能支持分布式部署的socket推送服务。
+    项目主页：<a href="https://github.com/cryhac/socket-service" target="_blank" rel="nofollow">https://github.com/cryhac/socket-service</a>
+    项目文档：<a href="https://github.com/cryhac/socket-service" target="_blank" rel="nofollow">https://github.com/cryhac/socket-service</a>
+<hr>
+
+<h3>scheduledTask-workerman</h3>
+    基于workerman和yaf开发的计划任务系统
+    项目主页：<a href="https://github.com/moxiaobai/scheduledTask-workerman" target="_blank" rel="nofollow">https://github.com/moxiaobai/scheduledTask-workerman</a>
+    项目文档：<a href="https://github.com/moxiaobai/scheduledTask-workerman" target="_blank" rel="nofollow">https://github.com/moxiaobai/scheduledTask-workerman</a>
+<hr>
+<h3>iyov http代理</h3>
+    Web proxy for http(s) for developers to analyze data between client and servers based on workerman, especailly for app developers.
+    项目主页：<a href="https://github.com/nicecp/iyov" target="_blank" rel="nofollow">https://github.com/nicecp/iyov</a>
+    项目文档：<a href="https://github.com/nicecp/iyov" target="_blank" rel="nofollow">https://github.com/nicecp/iyov</a>
+<hr>
+<h3>laravel_worker</h3>
+    基于laravel&workerman，php实现websocket的双向即时通讯的最佳实践
+    项目主页：<a href="https://github.com/shellus/laravel_worker" target="_blank" rel="nofollow">https://github.com/shellus/laravel_worker</a>
+    项目文档：<a href="https://github.com/shellus/laravel_worker" target="_blank" rel="nofollow">https://github.com/shellus/laravel_worker</a>
+<hr>
+<h3>workerman-thrift-resque</h3>
+    Thrift RPC and Resque based on workerman.
+    项目主页：<a href="https://github.com/vtumi/workerman-thrift-resque" target="_blank" rel="nofollow">https://github.com/vtumi/workerman-thrift-resque</a>
+    项目文档：<a href="https://github.com/vtumi/workerman-thrift-resque" target="_blank" rel="nofollow">https://github.com/vtumi/workerman-thrift-resque</a>
+<hr>
+<h3>workerman-statistics-java</h3>
+    一个分布式统计监控系统 包含JAVA客户端 、服务端
+    项目主页：<a href="https://github.com/shuiguang/workerman-statistics-java" target="_blank" rel="nofollow">https://github.com/shuiguang/workerman-statistics-java</a>
+    项目文档：<a href="https://github.com/shuiguang/workerman-statistics-java" target="_blank" rel="nofollow">https://github.com/shuiguang/workerman-statistics-java</a>
+<hr>
+<h3>CI-worker</h3>
+    workerman写的CI框架
+    项目主页：<a href="https://github.com/tmtbe/CI-worker" target="_blank" rel="nofollow">https://github.com/tmtbe/CI-worker</a>
+    项目文档：<a href="https://github.com/tmtbe/CI-worker" target="_blank" rel="nofollow">https://github.com/tmtbe/CI-worker</a>
+<hr>
+
+<h3>LoggerSever</h3>
+    基于workerman的日志服务器，采用udp上传日志，对应用性能没有影响，日志服务器支持多进程，符合psr-3日志规范。
+    项目主页：<a href="https://github.com/tmtbe/LoggerSever" target="_blank" rel="nofollow">https://github.com/tmtbe/LoggerSever</a>
+    项目文档：<a href="https://github.com/tmtbe/LoggerSever" target="_blank" rel="nofollow">https://github.com/tmtbe/LoggerSever</a>
+<hr>
+<h3>workerman-crontab</h3>
+    Dynamic crontab for php, power by workerman
+    项目主页：<a href="https://github.com/shuiguang/workerman-crontab" target="_blank" rel="nofollow">https://github.com/shuiguang/workerman-crontab</a>
+    项目文档：<a href="https://github.com/shuiguang/workerman-crontab" target="_blank" rel="nofollow">https://github.com/shuiguang/workerman-crontab</a>
+<hr>
+<h3>Workerman-ThinkPHP-Redis</h3>
+    workerman-chat+ThinkPHP+Redis
+    项目主页：<a href="https://github.com/happyliu2014/Workerman-ThinkPHP-Redis" target="_blank" rel="nofollow">https://github.com/happyliu2014/Workerman-ThinkPHP-Redis</a>
+    项目文档：<a href="https://github.com/happyliu2014/Workerman-ThinkPHP-Redis" target="_blank" rel="nofollow">https://github.com/happyliu2014/Workerman-ThinkPHP-Redis</a>
+<hr>
+<h3>Quasar</h3>
+    a Push Server for Nova Framework
+    项目主页：<a href="https://github.com/nova-framework/quasar" target="_blank" rel="nofollow">https://github.com/nova-framework/quasar</a>
+    项目文档：<a href="https://github.com/nova-framework/quasar" target="_blank" rel="nofollow">https://github.com/nova-framework/quasar</a>
+<hr>
