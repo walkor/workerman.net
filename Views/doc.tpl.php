efc2d19753d595929fadca57a4427f03e8d88ed2
walkor <walkor@users.noreply.github.com>
2018-05-29 18:05:44 +0800

    Update doc.tpl.php

--- Views/doc.tpl.php
+++ Views/doc.tpl.php
@@ -9,7 +9,7 @@
 					<a target="_blank" href="http://doc.workerman.net/">Workerman3.x版本手册</a>
 				</h4>
 				<p>Workerman是一个高性能的socket服务器通讯框架，用于快速开发各种网络应用，包括tcp的、udp的、长连接、短连接应用。
-				<br><a target="_blank" class="btn" href="http://doc.workerman.net/">在线手册 »</a> <a target="_blank" rel="nofollow" class="btn" href="http://www.kancloud.cn/walkor/workerman">离线手册 »</a>
+				<br><a target="_blank" class="btn" href="http://doc.workerman.net/">在线手册 »</a> 
 				</p>
 			</div>
 		</div>
@@ -25,7 +25,6 @@
 				可用于开发IM聊天应用、移动通讯、游戏后台、物联网、智能家居后台等等。<br>
 				<br>
 				<a class="btn" target="_blank" href="http://doc2.workerman.net/">GatewayWorker手册</a>
-				<a target="_blank" rel="nofollow" class="btn" href="https://www.kancloud.cn/walkor/gateway-worker">离线手册 »</a>
 				</p>
 			</div>
 		</div>
