81cbb14b0bd0fb8de4fc44f2c9ddf13e8dccbd09
walkor <walkor@users.noreply.github.com>
2018-07-23 14:54:20 +0800

    Update home.tpl.php

--- Views/home.tpl.php
+++ Views/home.tpl.php
@@ -453,6 +453,9 @@ echo $donate_list;
 		</h4>
 		<ul>
 			<li>
+				<a href="https://github.com/walkor/mqtt" rel="nofollow">2018-07-20 新增物联网 mqtt客户端</a>
+			</li>
+			<li>
 				<a href="http://www.workerman.net/last_update" rel="nofollow">2018-05-10 gatewayWorker3.0.8发布，增加一些常用uid接口</a>
 			</li>
 			<li>
