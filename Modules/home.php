677969795a7c4450e0eed9aea2c37d2bda7b5054
walkor <walkor@users.noreply.github.com>
2018-08-31 11:24:46 +0800

    Update home.php

--- Modules/home.php
+++ Modules/home.php
@@ -7,9 +7,9 @@ function home()
     {
         return _header('Location: /', 301);
     }
-    $html_title = 'workerman一个高性能 PHP Socket 服务器框架';
-    $html_keywords = 'workerman, php socket, php websocket';
-    $html_desc = 'Workerman是一款纯PHP开发的开源高性能的PHP socket服务器框架。支持TCP长连接，支持Websocket等诸多协议。被广泛的用于手机app、手游服务端、网络游戏服务器、聊天室、硬件通讯、智能家居、车联网、物联网等领域的开发。';
+    $html_title = 'workerman 高性能异步PHP Socket框架';
+    $html_keywords = 'workerman, php socket, php websocket, php异步';
+    $html_desc = 'Workerman是一款纯PHP开发的开源高性能的异步PHP socket框架。支持TCP长连接，支持websocket、MQTT等诸多协议。被广泛的用于手机app、手游服务端、网络游戏服务器、聊天室、硬件通讯、智能家居、车联网、物联网等领域的开发。';
     $html_nav = 'home';
     $download_workerman_count_key = 'download_workerman_count';
     $workerman_download_count =  \WorkerMan\Lib\Store::get($download_workerman_count_key);
