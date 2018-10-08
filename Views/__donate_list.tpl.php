cc95508ee05702be23903e8d5eb14a0c265f85ad
walkor <walkor@users.noreply.github.com>
2018-10-08 23:41:25 +0800

    Update __donate_list.tpl.php

--- Views/__donate_list.tpl.php
+++ Views/__donate_list.tpl.php
@@ -1,5 +1,6 @@
 <table class="table">
 	<tbody>
+		<tr class="active"><td>99¥</td><td>梦醒的夏天</td><td>2018-10-08</td><td>微信</td></tr>
 		<tr class="active"><td>10¥</td><td>匿名</td><td>2018-09-22</td><td>微信</td></tr>
 		<tr class="active"><td>¥5.50</td><td>莱芜市顺江木业有限公司</td><td>2018-09-19</td><td>支付宝</td></tr>
 		<tr class="active"><td>8.88¥</td><td>匿名</td><td>2018-09-13</td><td>微信</td></tr>
