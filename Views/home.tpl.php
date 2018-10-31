d0e0aac39db03d64bd092cd10c217f1092a1def8
walkor <walkor@users.noreply.github.com>
2018-10-31 10:39:59 +0800

    Update home.tpl.php

--- Views/home.tpl.php
+++ Views/home.tpl.php
@@ -95,7 +95,7 @@
 				<h4 class="media-heading">
 					<a target="_blank" href="https://www.wolive.cc">WoLive开源客服系统源码</a> <font style="color:red;font-size:13px">推荐</font>
 				</h4>
-				<p>Workerman开发的客服系统，任何站点引入一段js即可接入，接入成本极低。支持web站点和移动站点，支持表情、传图、传文件、粘贴截图、离线消息、客服管理、留言、历史记录、会话统计，支持多站点、支持万人在线。<br>
+				<p>Workerman开发的客服系统，任何站点引入一段js即可接入，接入成本极低。支持web站点和移动站点，支持表情、传图、传文件、粘贴截图、离线消息、客服管理、留言、历史记录、会话统计，支持多站点。<br>
 				<a class="btn" target="_blank" href="https://www.wolive.cc/" >开源客服系统源码</a>
 				</p>
 			</div>
