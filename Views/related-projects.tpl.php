9a7d00fc508673afb6ea951273b5596f874daaa5
walkor <walkor@users.noreply.github.com>
2019-11-01 23:23:16 +0800

    Update related-projects.tpl.php

--- Views/related-projects.tpl.php
+++ Views/related-projects.tpl.php
@@ -1,5 +1,5 @@
 <h2>workerman相关第三方项目</h2>
-<a href="http://wenda.workerman.net/?/question/2797">+添加自己的项目</a>
+<a href="http://wenda.workerman.net/question/2797">+添加自己的项目</a>
 <h3>Beanbun</h3>
     Beanbun 是用 PHP 编写的多进程网络爬虫框架，具有良好的开放性、高可扩展性，基于 Workerman。<br>
     项目主页：<a href="http://www.beanbun.org" target="_blank" rel="nofollow">http://www.beanbun.org</a><br>
@@ -164,4 +164,6 @@
     项目主页：<a href="https://github.com/mrtwenty/timer" target="_blank" rel="nofollow">https://github.com/mrtwenty/timer</a><br>
     项目文档：<a href="https://github.com/mrtwenty/timer" target="_blank" rel="nofollow">https://github.com/mrtwenty/timer</a>
 <hr>
-<h3><a href="https://wenda.workerman.net/question/2797">全部项目列表</a></h3>
+<h3>全部列表</h3>
+<a href="https://wenda.workerman.net/question/2797">全部项目列表</a>
+<a href="http://wenda.workerman.net/question/2797">+添加自己的项目</a>
