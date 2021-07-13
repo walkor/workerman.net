6fccdc8130e313ea299ecd3579d6e34944ab7a49
walkor <walkor@users.noreply.github.com>
2021-07-13 18:29:53 +0800

    Update footer.tpl.php

--- Views/footer.tpl.php
+++ Views/footer.tpl.php
@@ -27,6 +27,7 @@
 						<li><a href="/donate">捐赠</a></li>
 						<li><a href="https://www.wolive.cc" target="_blank">客服系统代码</a></li>
 						<li><a href="https://www.popoim.com" target="_blank">手机app聊天源码</a></li>
+						<li><a href="https://www.popoim.com/uniapp-chat.html" target="_blank">uniapp聊天源码</a></li>
 						<li><a href="https://www.99kf.com" target="_blank">客服系统源码</a></li>
                                                 <li><a href="https://www.popoim.cn" target="_blank">手机H5聊天源代码</a></li>
 						</ul>
